87175fcb7fa6078735d4fef53392f91e5b7de14e
Vincent Velociter <vincent.velociter@gmail.com>
2022-11-15 11:11:55 +0100

    Improve logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 
 import 'src/app.dart';
 import 'src/features/authentication/data/auth_repository.dart';
 
 void main() async {
+  if (kDebugMode) {
+    Logger.root.onRecord.listen((record) {
+      debugPrint(
+          '${record.level.name}: ${record.time}: ${record.message}; ${record.error != null ? record.error.toString() : ''}');
+    });
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
   final container = ProviderContainer();
   final authRepo = container.read(authRepositoryProvider);
--- lib/src/features/authentication/data/auth_repository.dart
+++ lib/src/features/authentication/data/auth_repository.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:developer' as developer;
+import 'package:logging/logging.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
@@ -16,21 +16,24 @@ const accountUrl = '$kLichessHost/api/account';
 const oauthScopes = ['board:play'];
 
 class AuthError {
-  AuthError(this.message);
+  AuthError(this.error);
 
-  final String message;
+  final Object error;
 }
 
 class AuthRepository {
   AuthRepository(
     FlutterAppAuth appAuth,
-    FlutterSecureStorage storage, {
+    FlutterSecureStorage storage,
+    Logger log, {
     required this.client,
   })  : _appAuth = appAuth,
-        _storage = storage;
+        _storage = storage,
+        _log = log;
 
   final AuthClient client;
 
+  final Logger _log;
   final FlutterAppAuth _appAuth;
   final FlutterSecureStorage _storage;
 
@@ -41,32 +44,31 @@ class AuthRepository {
 
   Future<void> init() {
     return _getToken().andThen(getAccount).match((_) {
-      developer.log('[AuthRepository] no account found during init');
+      _log.info('No account found during init');
     }, (account) {
       _authState.value = account;
     }).run();
   }
 
   TaskEither<AuthError, void> signIn() {
-    return TaskEither.tryCatch(
-      () async {
-        final result = await _appAuth.authorizeAndExchangeCode(AuthorizationTokenRequest(
-          lichessClientId,
-          redirectUri,
-          serviceConfiguration: const AuthorizationServiceConfiguration(
-              authorizationEndpoint: '$kLichessHost/oauth',
-              tokenEndpoint: '$kLichessHost/api/token'),
-          scopes: oauthScopes,
-        ));
-        if (result != null) {
-          developer.log('[AuthRepository] got accessToken ${result.accessToken}');
-          await _storage.write(key: lichessClientId, value: result.accessToken);
-        } else {
-          throw Exception('[AuthRepository] Could not login');
-        }
-      },
-      (error, _) => AuthError(error.toString()),
-    ).andThen(getAccount).map((account) {
+    return TaskEither.tryCatch(() async {
+      final result = await _appAuth.authorizeAndExchangeCode(AuthorizationTokenRequest(
+        lichessClientId,
+        redirectUri,
+        serviceConfiguration: const AuthorizationServiceConfiguration(
+            authorizationEndpoint: '$kLichessHost/oauth', tokenEndpoint: '$kLichessHost/api/token'),
+        scopes: oauthScopes,
+      ));
+      if (result != null) {
+        _log.fine('Got accessToken ${result.accessToken}');
+        await _storage.write(key: lichessClientId, value: result.accessToken);
+      } else {
+        throw Exception('FlutterAppAuth.authorizeAndExchangeCode error');
+      }
+    }, (error, trace) {
+      _log.severe('Could not sign in', error, trace);
+      return AuthError(error);
+    }).andThen(getAccount).map((account) {
       _authState.value = account;
     });
   }
@@ -78,7 +80,10 @@ class AuthRepository {
         await _storage.delete(key: lichessClientId);
         _authState.value = null;
       }
-    }, (error, _) => AuthError(error.toString()));
+    }, (error, trace) {
+      _log.severe('signOut error', error, trace);
+      return AuthError(error);
+    });
   }
 
   TaskEither<AuthError, Account> getAccount() {
@@ -88,9 +93,12 @@ class AuthRepository {
       if (response.statusCode == 200) {
         return Account.fromJson(jsonDecode(response.body));
       } else {
-        throw Exception('[AuthRepository] Could not get account (error ${response.statusCode})');
+        throw Exception('Error ${response.statusCode}');
       }
-    }, (error, _) => AuthError(error.toString()));
+    }, (error, trace) {
+      _log.warning('getAccount error', error, trace);
+      return AuthError(error);
+    });
   }
 
   void dispose() {
@@ -98,8 +106,11 @@ class AuthRepository {
     client.close();
   }
 
-  TaskEither<AuthError, String?> _getToken() => TaskEither.tryCatch(
-      () => _storage.read(key: lichessClientId), (error, _) => AuthError(error.toString()));
+  TaskEither<AuthError, String?> _getToken() =>
+      TaskEither.tryCatch(() => _storage.read(key: lichessClientId), (error, trace) {
+        _log.severe('_getToken error', error, trace);
+        return AuthError(error);
+      });
 }
 
 class AuthClient extends http.BaseClient {
@@ -112,7 +123,6 @@ class AuthClient extends http.BaseClient {
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
     final token = await storage.read(key: lichessClientId);
     request.headers['Authorization'] = 'Bearer ${(token ?? '')}';
-    developer.log('http authorization header: ${request.headers['Authorization']!}');
     return _inner.send(request);
   }
 }
@@ -121,7 +131,8 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
   const auth = FlutterAppAuth();
   const storage = FlutterSecureStorage();
   final client = AuthClient(http.Client(), storage);
-  final repo = AuthRepository(auth, storage, client: client);
+  final log = Logger('AuthRepository');
+  final repo = AuthRepository(auth, storage, log, client: client);
   ref.onDispose(() => repo.dispose());
   return repo;
 });
