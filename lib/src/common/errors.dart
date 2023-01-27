8c8458cc65c0293e727393a24a146f0c28d13ac7
Vincent Velociter <vincent.velociter@gmail.com>
2023-01-27 17:47:01 +0100

    Fix errors toString

--- lib/src/common/errors.dart
+++ lib/src/common/errors.dart
@@ -5,36 +5,36 @@ abstract class IOException implements Exception {
   String toString() => message;
 }
 
-class GenericIOException implements IOException {
+class GenericIOException extends IOException {
   @override
   String get message =>
       'The operation could not be completed. Please try again later.';
 }
 
 /// Exception when json returned by server is not valid.
-class DataFormatException implements IOException {
+class DataFormatException extends IOException {
   @override
   String get message => 'Could not read data from server.';
 }
 
 /// Generic error for API requests.
-class ApiRequestException implements IOException {
+class ApiRequestException extends IOException {
   @override
   String get message =>
       'Something went wrong with the request. Please try again later.';
 }
 
-class NotFoundException implements IOException {
+class NotFoundException extends IOException {
   @override
   String get message => 'Requested resource could not be found.';
 }
 
-class UnauthorizedException implements IOException {
+class UnauthorizedException extends IOException {
   @override
   String get message => 'You must sign in to access this resource.';
 }
 
-class ForbiddenException implements IOException {
+class ForbiddenException extends IOException {
   @override
   String get message => 'Requested resource access is forbidden.';
 }
