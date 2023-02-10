8ff16b09907a377deb494d0aba07252010c75b33
Vincent Velociter <vincent.velociter@gmail.com>
2023-02-10 10:36:09 +0100

    Remove unused import

--- test/ui/settings/settings_screen_test.dart
+++ test/ui/settings/settings_screen_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
