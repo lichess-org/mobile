5ae624d8a0f8c699cd633f3418029c6f7589f340
Benjamin Lehmann <bclehmann@yahoo.ca>
2023-02-13 15:12:41 -0700

    board_theme_screen: Thumbnails

--- lib/src/ui/settings/board_theme_screen.dart
+++ lib/src/ui/settings/board_theme_screen.dart
@@ -37,10 +37,41 @@ class BoardThemeScreen extends StatelessWidget {
 class _Body extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final pieceSet = ref.watch(pieceSetPrefProvider);
     final boardTheme = ref.watch(boardThemePrefProvider);
 
-    void onChanged(BoardTheme? value) =>
-        ref.read(boardThemePrefProvider.notifier).set(value ?? BoardTheme.brown);
+    List<Image> getPieceImages() {
+      return [
+        Image(
+          image: pieceSet.assets['whiteking']!,
+          height: 32,
+        ),
+        Image(
+          image: pieceSet.assets['blackqueen']!,
+          height: 32,
+        ),
+        Image(
+          image: pieceSet.assets['whiterook']!,
+          height: 32,
+        ),
+        Image(
+          image: pieceSet.assets['blackbishop']!,
+          height: 32,
+        ),
+        Image(
+          image: pieceSet.assets['whiteknight']!,
+          height: 32,
+        ),
+        Image(
+          image: pieceSet.assets['blackpawn']!,
+          height: 32,
+        )
+      ];
+    }
+
+    void onChanged(BoardTheme? value) => ref
+        .read(boardThemePrefProvider.notifier)
+        .set(value ?? BoardTheme.brown);
 
     return SafeArea(
       child: ListView(
@@ -53,6 +84,21 @@ class _Body extends ConsumerWidget {
             ),
             choices: BoardTheme.values,
             selectedItem: boardTheme,
+            leadingBuilder: (t) => ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 192,
+              ),
+              child: Stack(
+                children: [
+                  Image.asset(
+                    "assets/board-thumbnails/${t.name}.jpg",
+                    height: 32,
+                    errorBuilder: (context, o, st) => const SizedBox.shrink(),
+                  ),
+                  Row(children: getPieceImages())
+                ],
+              ),
+            ),
             titleBuilder: (t) => Text(t.label),
             onSelectedItemChanged: onChanged,
           )
