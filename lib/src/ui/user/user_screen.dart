b5e7fea6ede36a31549725e271d59b9ec8076e46
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-02-23 21:38:32 +0000

    removing button

--- lib/src/ui/user/user_screen.dart
+++ lib/src/ui/user/user_screen.dart
@@ -207,44 +207,7 @@ class PerfCards extends StatelessWidget {
     }).toList(growable: false);
 
     if (userPerfs.isEmpty) {
-      return Padding(
-        padding: Styles.bodySectionPadding,
-        child: SizedBox(
-          height: 106,
-          child: SizedBox(
-            height: 100,
-            width: 100,
-            child: GestureDetector(
-              onTap: () => _handlePlayGameCardTap(context),
-              child: PlatformCard(
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        context.l10n.playWithAFriend,
-                        // TODO: maybe put a new message to be translated
-                        style: TextStyle(color: textShade(context, 0.7)),
-                      ),
-                      Icon(
-                        LichessIcons.chess_king,
-                        color: textShade(context, 0.6),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: const [SizedBox(width: 3)],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
+      return Container();
     }
 
     return Padding(
