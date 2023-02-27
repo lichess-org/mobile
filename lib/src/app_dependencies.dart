e5f74e2727284c225460f6dfcca757ae46445a0f
Vincent Velociter <vincent.velociter@gmail.com>
2023-02-27 09:39:09 +0100

    Check server session on app init

--- lib/src/app_dependencies.dart
+++ lib/src/app_dependencies.dart
@@ -1,11 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:result_extensions/result_extensions.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:soundpool/soundpool.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
+import 'package:lichess_mobile/src/common/errors.dart';
 import 'package:lichess_mobile/src/common/sound_service.dart';
+import 'package:lichess_mobile/src/model/account/account_providers.dart';
 import 'package:lichess_mobile/src/model/auth/session_storage.dart';
 import 'package:lichess_mobile/src/model/auth/user_session.dart';
 
@@ -23,6 +26,18 @@ Future<AppDependencies> appDependencies(
   final sounds = await loadSounds(pool);
 
   ref.onDispose(pool.release);
+
+  final accountRepository = ref.watch(accountRepositoryProvider);
+  final storedSession = await sessionStorage.read();
+  if (storedSession != null) {
+    await accountRepository.getProfile().match(
+      onError: (err, _) {
+        if (err is UnauthorizedException) {
+          sessionStorage.delete();
+        }
+      },
+    );
+  }
 
   return AppDependencies(
     packageInfo: pInfo,
