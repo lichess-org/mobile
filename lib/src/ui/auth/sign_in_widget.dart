09ee0a213474a53ea5c2c07059909282d5997810
Vincent Velociter <vincent.velociter@gmail.com>
2023-02-27 09:42:18 +0100

    Fix sign in button not showing on android

--- lib/src/ui/auth/sign_in_widget.dart
+++ lib/src/ui/auth/sign_in_widget.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/utils/async_value.dart';
 
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
-import 'package:lichess_mobile/src/model/settings/providers.dart';
 import 'package:lichess_mobile/src/model/auth/auth_controller.dart';
 import 'package:lichess_mobile/src/model/auth/user_session.dart';
 
@@ -14,7 +12,6 @@ class SignInWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authController = ref.watch(authControllerProvider);
-    final brightness = ref.watch(currentBrightnessProvider);
     final session = ref.watch(userSessionStateProvider);
 
     ref.listen<AsyncValue<void>>(
@@ -28,10 +25,6 @@ class SignInWidget extends ConsumerWidget {
           : () => ref.read(authControllerProvider.notifier).signIn(),
       child: Text(
         context.l10n.signIn,
-        style: defaultTargetPlatform == TargetPlatform.android &&
-                brightness == Brightness.light
-            ? const TextStyle(color: Colors.white)
-            : null,
       ),
     );
     return session == null ? button : const SizedBox.shrink();
