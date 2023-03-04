c4e0b46e7c7c330ec19b63965c183073ecefd4e0
brandonpl8 <brandonpaullai@gmail.com>
2023-03-03 22:36:01 -0800

    Game provider

--- lib/src/model/tv/tv_repository.dart
+++ lib/src/model/tv/tv_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lichess_mobile/src/common/models.dart';
 import 'package:logging/logging.dart';
 
 import 'package:lichess_mobile/src/common/api_client.dart';
@@ -35,6 +36,41 @@ class TvRepository {
 final tvRepositoryProvider = Provider<TvRepository>((ref) {
   final apiClient = ref.watch(apiClientProvider);
   final repo = TvRepository(Logger('TvRepository'), apiClient: apiClient);
+  ref.onDispose(() => repo.dispose());
+  return repo;
+});
+
+class TvStreamRepository {
+  const TvStreamRepository(
+    Logger log, {
+    required this.apiClient,
+  }) : _log = log;
+
+  final ApiClient apiClient;
+  final Logger _log;
+
+  Stream<TvEvent> tvFeed(GameId gameId) async* {
+    final resp = await apiClient
+        .stream(Uri.parse('$kLichessHost/api/stream/game/$gameId'));
+    _log.fine('Start streaming TV.');
+    yield* resp.stream
+        .toStringStream()
+        .where((event) => event.isNotEmpty && event != '\n')
+        .map((event) => jsonDecode(event) as Map<String, dynamic>)
+        .where((json) => json['t'] == 'featured' || json['t'] == 'fen')
+        .map((json) => TvEvent.fromJson(json))
+        .handleError((Object error) => _log.warning(error));
+  }
+
+  void dispose() {
+    apiClient.close();
+  }
+}
+
+final tvStreamProvider = Provider<TvStreamRepository>((ref) {
+  final apiClient = ref.watch(apiClientProvider);
+  final repo =
+      TvStreamRepository(Logger('TvStreamRepository'), apiClient: apiClient);
   ref.onDispose(() => repo.dispose());
   return repo;
 });
