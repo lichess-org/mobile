96b0117458022e1754c2b5d235d37955877db212
nav-28 <pwadhwa@ualberta.ca>
2023-04-25 19:57:24 -0600

    replace with cg side

--- lib/src/ui/puzzle/puzzle_history_widget.dart
+++ lib/src/ui/puzzle/puzzle_history_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
@@ -9,10 +10,10 @@ import 'package:lichess_mobile/src/model/puzzle/puzzle.dart';
 import 'package:lichess_mobile/src/model/puzzle/puzzle_providers.dart';
 import 'package:lichess_mobile/src/model/puzzle/puzzle_history_storage.dart';
 import 'package:lichess_mobile/src/utils/navigation.dart';
+import 'package:lichess_mobile/src/utils/chessground_compat.dart';
 import 'package:lichess_mobile/src/widgets/list.dart';
 import 'package:lichess_mobile/src/widgets/board_preview.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
-import 'package:chessground/chessground.dart';
 import 'package:lichess_mobile/src/widgets/platform.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -78,7 +79,7 @@ class _HistoryList extends StatelessWidget {
           builder: (ctx, constrains) {
             final boardWidth = constrains.maxWidth / 2;
             return SizedBox(
-              height: boardWidth * 2,
+              height: boardWidth * 2 + 50,
               child: GridView.count(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
@@ -89,10 +90,7 @@ class _HistoryList extends StatelessWidget {
                     width: boardWidth,
                     height: boardWidth + 30, // + 30 for text Widget
                     child: BoardPreview(
-                      orientation: preview.orientation == Side.white
-                          ? Side.white
-                          : Side
-                              .white, // conflict with dartchess and chessground on Side
+                      orientation: preview.orientation.cg,
                       fen: preview.initialFen,
                       footer: Text(
                         puzzle.result ? 'Solved' : 'Failed',
@@ -123,7 +121,26 @@ class HistoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return PlatformWidget(
       androidBuilder: _buildAndroid,
-      iosBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    final boardWidth = MediaQuery.of(context).size.width / 2;
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(context.l10n.puzzleHistory),
+      ),
+      child: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: history
+              .map(
+                (e) => HistoryColumn(e!, boardWidth),
+              )
+              .toList(),
+        ),
+      )),
     );
   }
 
@@ -156,51 +173,44 @@ class HistoryColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 6),
-      child: ListSection(
-        trailingWidget: Text(
-          timeago.format(DateTime.parse(history.date)),
-          style: const TextStyle(
-            fontWeight: FontWeight.w300,
-            color: LichessColors.grey,
-          ),
+    return ListSection(
+      trailingWidget: Text(
+        timeago.format(DateTime.parse(history.date)),
+        style: const TextStyle(
+          fontWeight: FontWeight.w300,
+          color: LichessColors.grey,
         ),
-        header: Row(
-          children: [
-            Text(puzzleThemeL10n(context, history.angle).name),
-          ],
-        ),
+      ),
+      header: Row(
         children: [
-          GridView.count(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            children: history.puzzles.map((puzzle) {
-              final preview = PuzzlePreview.fromPuzzle(puzzle.puzzle);
-              return SizedBox(
-                width: boardWidth,
-                height: boardWidth + 30, // + 30 for text Widget
-                child: BoardPreview(
-                  orientation: preview.orientation == Side.white
-                      ? Side.white
-                      : Side
-                          .white, // conflict with dartchess and chessground on Side
-                  fen: preview.initialFen,
-                  footer: Text(
-                    puzzle.result ? 'Solved' : 'Failed',
-                    style: TextStyle(
-                      color: puzzle.result
-                          ? LichessColors.good
-                          : LichessColors.red,
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
-          )
+          Text(puzzleThemeL10n(context, history.angle).name),
         ],
       ),
+      children: [
+        GridView.count(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          children: history.puzzles.map((puzzle) {
+            final preview = PuzzlePreview.fromPuzzle(puzzle.puzzle);
+            return SizedBox(
+              width: boardWidth,
+              height: boardWidth + 30, // + 30 for text Widget
+              child: BoardPreview(
+                orientation: preview.orientation.cg,
+                fen: preview.initialFen,
+                footer: Text(
+                  puzzle.result ? 'Solved' : 'Failed',
+                  style: TextStyle(
+                    color:
+                        puzzle.result ? LichessColors.good : LichessColors.red,
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        )
+      ],
     );
   }
 }
