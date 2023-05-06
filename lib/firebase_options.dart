70cf1aaaa9a70cba869c25c11641f0725ce3ee00
Vincent Velociter <vincent.velociter@gmail.com>
2023-05-06 14:12:39 -0300

    Fix format

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -63,7 +63,8 @@ class DefaultFirebaseOptions {
     messagingSenderId: '974101866555',
     projectId: 'lichessv2',
     storageBucket: 'lichessv2.appspot.com',
-    iosClientId: '974101866555-8ag66ua0p0pn1ab7u982i58a9iubhbod.apps.googleusercontent.com',
+    iosClientId:
+        '974101866555-8ag66ua0p0pn1ab7u982i58a9iubhbod.apps.googleusercontent.com',
     iosBundleId: 'org.lichess.mobileV2',
   );
 }
