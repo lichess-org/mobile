f47b848988357914b2660d97fcb888afd8d987e9
Bl4ckspell7 <robertheyd@proton.me>
2023-05-09 16:42:55 +0200

    formatted

--- lib/src/widgets/bottom_navigation.dart
+++ lib/src/widgets/bottom_navigation.dart
@@ -261,13 +261,13 @@ class _TabSwitchingViewState extends State<_TabSwitchingView> {
 
     final List<DisplayMode> sameResolution = supported
         .where((DisplayMode m) =>
-    m.width == active.width && m.height == active.height)
+            m.width == active.width && m.height == active.height)
         .toList()
       ..sort((DisplayMode a, DisplayMode b) =>
           b.refreshRate.compareTo(a.refreshRate));
 
     final DisplayMode mostOptimalMode =
-    sameResolution.isNotEmpty ? sameResolution.first : active;
+        sameResolution.isNotEmpty ? sameResolution.first : active;
 
     // This setting is per session.
     // Please ensure this was placed with `initState` of your root widget.
