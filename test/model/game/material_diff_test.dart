c8d2c3d4252aa0da1a0be51a9c13f906d104b813
ZTL-UwU <zhangtianli2006@163.com>
2023-05-10 16:44:07 +0800

    add material diff test
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ test/model/game/material_diff_test.dart
@@ -0,0 +1,43 @@
+import 'package:dartchess/dartchess.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lichess_mobile/src/model/game/game.dart';
+
+void main() {
+  group('GameMaterialDiff', () {
+    test('generation from board', () {
+      final Board board =
+          Board.parseFen("r5k1/3Q1pp1/2p4p/4P1b1/p3R3/3P4/6PP/R5K1");
+      final MaterialDiff diff = MaterialDiff.fromBoard(board);
+
+      expect(diff.bySide(Side.black).score, equals(-10));
+      expect(diff.bySide(Side.white).score, equals(10));
+      expect(
+        diff.bySide(Side.black).pieces,
+        equals(
+          IMap<Role, int>(const {
+            Role.king: 0,
+            Role.queen: 0,
+            Role.rook: 0,
+            Role.bishop: 1,
+            Role.knight: 0,
+            Role.pawn: 1,
+          }),
+        ),
+      );
+      expect(
+        diff.bySide(Side.white).pieces,
+        equals(
+          IMap<Role, int>(const {
+            Role.king: 0,
+            Role.queen: 1,
+            Role.rook: 1,
+            Role.bishop: 0,
+            Role.knight: 0,
+            Role.pawn: 0,
+          }),
+        ),
+      );
+    });
+  });
+}
