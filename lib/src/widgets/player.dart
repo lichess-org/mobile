8891fed2c83f9a79827aa37f0216e73f40e45507
ZTL-UwU <zhangtianli2006@163.com>
2023-05-10 16:45:37 +0800

    remove unused import
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- lib/src/widgets/player.dart
+++ lib/src/widgets/player.dart
@@ -1,5 +1,4 @@
 import 'package:dartchess/dartchess.dart';
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:lichess_mobile/src/model/game/game.dart';
 
