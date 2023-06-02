eb7eb1579852f5339a3045ffd26c18eb29dda5d5
Vincent Velociter <vincent.velociter@gmail.com>
2023-06-02 11:41:00 +0200

    Tweak ios no padding button

--- lib/src/widgets/buttons.dart
+++ lib/src/widgets/buttons.dart
@@ -117,6 +117,8 @@ class AppBarTextButton extends StatelessWidget {
   }
 }
 
+/// Button that explicitly reduce padding, thus does not conform to accessibility
+/// guidelines. So use sparingly.
 class NoPaddingTextButton extends StatelessWidget {
   const NoPaddingTextButton({
     required this.child,
@@ -133,6 +135,7 @@ class NoPaddingTextButton extends StatelessWidget {
         ? CupertinoButton(
             padding: EdgeInsets.zero,
             onPressed: onPressed,
+            minSize: 23,
             child: child,
           )
         : TextButton(
