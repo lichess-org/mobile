49190ebaad3fe2febc42f90592af6430f38c7cd2
Vincent Velociter <vincent.velociter@gmail.com>
2023-06-12 15:12:25 +0200

    Debounce tv stream requests to avoid 429

--- lib/src/utils/riverpod.dart
+++ lib/src/utils/riverpod.dart
@@ -10,3 +10,21 @@ extension AutoDisposeRefExtension<T> on AutoDisposeRef<T> {
     return link;
   }
 }
+
+extension RefExtension on Ref {
+  /// Delays an execution by a bit such that if a dependency changes multiple
+  /// time rapidly, the rest of the code is only run once.
+  Future<void> debounce(Duration duration) {
+    final completer = Completer<void>();
+    final timer = Timer(duration, () {
+      if (!completer.isCompleted) completer.complete();
+    });
+    onDispose(() {
+      timer.cancel();
+      if (!completer.isCompleted) {
+        completer.completeError(StateError('Cancelled'));
+      }
+    });
+    return completer.future;
+  }
+}
