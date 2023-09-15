9f3bae5a7cf9389e445482cb87d73ee0f583beca
papo1011 <francesco.papini.siena@gmail.com>
2023-09-15 22:39:38 +0200

    feat: init RelationRepository with getFollowing()

---
+++ lib/src/model/relation/relation_repository.dart
@@ -0,0 +1,32 @@
+import 'package:logging/logging.dart';
+import 'package:result_extensions/result_extensions.dart';
+
+import 'package:lichess_mobile/src/model/auth/auth_client.dart';
+import 'package:lichess_mobile/src/constants.dart';
+import 'package:lichess_mobile/src/utils/json.dart';
+import 'package:lichess_mobile/src/model/user/user.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+
+class RelationRepository {
+  const RelationRepository({required this.apiClient, required Logger logger})
+      : _log = logger;
+
+  final AuthClient apiClient;
+  final Logger _log;
+
+  FutureResult<IList<User>> getFollowing() async {
+    return apiClient
+        .get(
+          Uri.parse('$kLichessHost/api/rel/following'),
+        )
+        .then(
+          (result) => result.flatMap(
+            (response) => readJsonListOfObjects(
+              response,
+              mapper: (json) => User.fromJson(json),
+              logger: _log,
+            ),
+          ),
+        );
+  }
+}
