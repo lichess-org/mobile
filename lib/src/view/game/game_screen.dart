2cc89f38677d7e3662a8555d0cc1fa4478feace6
Vincent Velociter <vincent.velociter@gmail.com>
2023-10-02 09:32:16 +0200

    Extract GameSettings to it own file

--- lib/src/view/game/game_screen.dart
+++ lib/src/view/game/game_screen.dart
@@ -14,8 +14,6 @@ import 'package:lichess_mobile/src/model/game/game_repository_providers.dart';
 import 'package:lichess_mobile/src/model/lobby/lobby_game.dart';
 import 'package:lichess_mobile/src/model/lobby/game_seek.dart';
 import 'package:lichess_mobile/src/model/settings/play_preferences.dart';
-import 'package:lichess_mobile/src/model/settings/board_preferences.dart';
-import 'package:lichess_mobile/src/model/settings/general_preferences.dart';
 import 'package:lichess_mobile/src/model/user/user_repository_providers.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
 import 'package:lichess_mobile/src/utils/navigation.dart';
@@ -28,7 +26,6 @@ import 'package:lichess_mobile/src/widgets/board_table.dart';
 import 'package:lichess_mobile/src/widgets/countdown_clock.dart';
 import 'package:lichess_mobile/src/widgets/player.dart';
 import 'package:lichess_mobile/src/widgets/yes_no_dialog.dart';
-import 'package:lichess_mobile/src/widgets/settings.dart';
 import 'package:lichess_mobile/src/utils/immersive_mode.dart';
 import 'package:lichess_mobile/src/utils/wakelock.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
@@ -37,6 +34,7 @@ import 'package:lichess_mobile/src/utils/chessground_compat.dart';
 import 'game_screen_providers.dart';
 import 'ping_rating.dart';
 import 'game_loader.dart';
+import 'game_settings.dart';
 import 'status_l10n.dart';
 
 final RouteObserver<PageRoute<void>> gameRouteObserver =
@@ -183,7 +181,7 @@ class _GameScreenState extends ConsumerState<GameScreen>
             onPressed: () => showAdaptiveBottomSheet<void>(
               context: context,
               isScrollControlled: true,
-              builder: (_) => _GameSettings(ctrlProvider),
+              builder: (_) => GameSettings(ctrlProvider),
             ),
           ),
         ],
@@ -213,7 +211,7 @@ class _GameScreenState extends ConsumerState<GameScreen>
           onPressed: () => showAdaptiveBottomSheet<void>(
             context: context,
             isScrollControlled: true,
-            builder: (_) => _GameSettings(ctrlProvider),
+            builder: (_) => GameSettings(ctrlProvider),
           ),
         ),
       ),
@@ -454,92 +452,6 @@ class _Body extends ConsumerWidget {
             .rematch(state.requireValue.redirectGameId!);
       }
     }
-  }
-}
-
-class _GameSettings extends ConsumerWidget {
-  const _GameSettings(this.ctrlProvider);
-
-  final GameCtrlProvider? ctrlProvider;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isSoundEnabled = ref.watch(
-      generalPreferencesProvider.select(
-        (prefs) => prefs.isSoundEnabled,
-      ),
-    );
-    final boardPrefs = ref.watch(boardPreferencesProvider);
-    final gameState = ctrlProvider != null
-        ? ref.watch(ctrlProvider!)
-        : const AsyncValue<GameCtrlState>.loading();
-
-    return ModalSheetScaffold(
-      title: Text(context.l10n.settingsSettings),
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          Padding(
-            padding: Styles.bodyPadding,
-            child: Text(
-              context.l10n.preferencesPreferences,
-              style: Styles.title,
-            ),
-          ),
-          SwitchSettingTile(
-            title: Text(context.l10n.sound),
-            value: isSoundEnabled,
-            onChanged: (value) {
-              ref
-                  .read(generalPreferencesProvider.notifier)
-                  .toggleSoundEnabled();
-            },
-          ),
-          SwitchSettingTile(
-            title: const Text('Haptic feedback'),
-            value: boardPrefs.hapticFeedback,
-            onChanged: (value) {
-              ref
-                  .read(boardPreferencesProvider.notifier)
-                  .toggleHapticFeedback();
-            },
-          ),
-          SwitchSettingTile(
-            title: Text(
-              context.l10n.preferencesPieceAnimation,
-              maxLines: 2,
-            ),
-            value: boardPrefs.pieceAnimation,
-            onChanged: (value) {
-              ref
-                  .read(boardPreferencesProvider.notifier)
-                  .togglePieceAnimation();
-            },
-          ),
-          gameState.when(
-            data: (data) {
-              if (data.game.prefs?.submitMove == true) {
-                return SwitchSettingTile(
-                  title: Text(
-                    context.l10n.preferencesMoveConfirmation,
-                    maxLines: 2,
-                  ),
-                  value: data.shouldConfirmMove,
-                  onChanged: (value) {
-                    ref.read(ctrlProvider!.notifier).toggleMoveConfirmation();
-                  },
-                );
-              } else {
-                return const SizedBox.shrink();
-              }
-            },
-            loading: () => const SizedBox.shrink(),
-            error: (e, s) => const SizedBox.shrink(),
-          ),
-          const SizedBox(height: 16.0),
-        ],
-      ),
-    );
   }
 }
 
---
+++ lib/src/view/game/game_settings.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:lichess_mobile/src/model/game/game_ctrl.dart';
+import 'package:lichess_mobile/src/model/settings/board_preferences.dart';
+import 'package:lichess_mobile/src/model/settings/general_preferences.dart';
+import 'package:lichess_mobile/src/styles/styles.dart';
+import 'package:lichess_mobile/src/widgets/adaptive_bottom_sheet.dart';
+import 'package:lichess_mobile/src/widgets/settings.dart';
+import 'package:lichess_mobile/src/utils/l10n_context.dart';
+
+class GameSettings extends ConsumerWidget {
+  const GameSettings(this.ctrlProvider, {super.key});
+
+  final GameCtrlProvider? ctrlProvider;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isSoundEnabled = ref.watch(
+      generalPreferencesProvider.select(
+        (prefs) => prefs.isSoundEnabled,
+      ),
+    );
+    final boardPrefs = ref.watch(boardPreferencesProvider);
+    final gameState = ctrlProvider != null
+        ? ref.watch(ctrlProvider!)
+        : const AsyncValue<GameCtrlState>.loading();
+
+    return ModalSheetScaffold(
+      title: Text(context.l10n.settingsSettings),
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Padding(
+            padding: Styles.bodyPadding,
+            child: Text(
+              context.l10n.preferencesPreferences,
+              style: Styles.title,
+            ),
+          ),
+          SwitchSettingTile(
+            title: Text(context.l10n.sound),
+            value: isSoundEnabled,
+            onChanged: (value) {
+              ref
+                  .read(generalPreferencesProvider.notifier)
+                  .toggleSoundEnabled();
+            },
+          ),
+          SwitchSettingTile(
+            title: const Text('Haptic feedback'),
+            value: boardPrefs.hapticFeedback,
+            onChanged: (value) {
+              ref
+                  .read(boardPreferencesProvider.notifier)
+                  .toggleHapticFeedback();
+            },
+          ),
+          SwitchSettingTile(
+            title: Text(
+              context.l10n.preferencesPieceAnimation,
+              maxLines: 2,
+            ),
+            value: boardPrefs.pieceAnimation,
+            onChanged: (value) {
+              ref
+                  .read(boardPreferencesProvider.notifier)
+                  .togglePieceAnimation();
+            },
+          ),
+          gameState.when(
+            data: (data) {
+              if (data.game.prefs?.submitMove == true) {
+                return SwitchSettingTile(
+                  title: Text(
+                    context.l10n.preferencesMoveConfirmation,
+                    maxLines: 2,
+                  ),
+                  value: data.shouldConfirmMove,
+                  onChanged: (value) {
+                    ref.read(ctrlProvider!.notifier).toggleMoveConfirmation();
+                  },
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            },
+            loading: () => const SizedBox.shrink(),
+            error: (e, s) => const SizedBox.shrink(),
+          ),
+          const SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+}
