fca351a47afcadf8f53c50f54f706c5a492b803e
Vincent Velociter <vincent.velociter@gmail.com>
2023-10-05 09:57:42 +0200

    Move AnalysisSettings to its own file

--- lib/src/view/analysis/analysis_screen.dart
+++ lib/src/view/analysis/analysis_screen.dart
@@ -16,7 +16,6 @@ import 'package:lichess_mobile/src/model/common/node.dart';
 import 'package:lichess_mobile/src/model/engine/engine_evaluation.dart';
 import 'package:lichess_mobile/src/model/analysis/analysis_controller.dart';
 import 'package:lichess_mobile/src/model/settings/board_preferences.dart';
-import 'package:lichess_mobile/src/model/settings/general_preferences.dart';
 import 'package:lichess_mobile/src/model/settings/brightness.dart';
 import 'package:lichess_mobile/src/model/settings/analysis_preferences.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
@@ -25,13 +24,12 @@ import 'package:lichess_mobile/src/widgets/adaptive_action_sheet.dart';
 import 'package:lichess_mobile/src/widgets/adaptive_bottom_sheet.dart';
 import 'package:lichess_mobile/src/widgets/buttons.dart';
 import 'package:lichess_mobile/src/widgets/list.dart';
-import 'package:lichess_mobile/src/widgets/non_linear_slider.dart';
 import 'package:lichess_mobile/src/widgets/platform.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
 import 'package:lichess_mobile/src/utils/chessground_compat.dart';
-import 'package:lichess_mobile/src/widgets/settings.dart';
 
 import 'tree_view.dart';
+import 'analysis_settings.dart';
 
 class AnalysisScreen extends ConsumerWidget {
   const AnalysisScreen({
@@ -63,7 +61,7 @@ class AnalysisScreen extends ConsumerWidget {
             onPressed: () => showAdaptiveBottomSheet<void>(
               context: context,
               isScrollControlled: true,
-              builder: (_) => _AnalysisSettings(ctrlProvider),
+              builder: (_) => AnalysisSettings(ctrlProvider),
             ),
           ),
         ],
@@ -86,7 +84,7 @@ class AnalysisScreen extends ConsumerWidget {
               onPressed: () => showAdaptiveBottomSheet<void>(
                 context: context,
                 isScrollControlled: true,
-                builder: (_) => _AnalysisSettings(ctrlProvider),
+                builder: (_) => AnalysisSettings(ctrlProvider),
               ),
             ),
           ],
@@ -668,124 +666,6 @@ class _StockfishInfo extends ConsumerWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class _AnalysisSettings extends ConsumerWidget {
-  const _AnalysisSettings(this.ctrlProvider);
-
-  final AnalysisControllerProvider ctrlProvider;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(ctrlProvider);
-    final prefs = ref.watch(analysisPreferencesProvider);
-    final isSoundEnabled = ref.watch(
-      generalPreferencesProvider.select((pref) => pref.isSoundEnabled),
-    );
-
-    return ModalSheetScaffold(
-      title: Text(context.l10n.analysisOptions),
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          const SizedBox(height: 8.0),
-          SwitchSettingTile(
-            title: Text(context.l10n.toggleLocalEvaluation),
-            value: prefs.enableLocalEvaluation,
-            onChanged: state.isLocalEvaluationAllowed
-                ? (_) {
-                    ref.read(ctrlProvider.notifier).toggleLocalEvaluation();
-                  }
-                : null,
-          ),
-          PlatformListTile(
-            title: Text.rich(
-              TextSpan(
-                text: '${context.l10n.multipleLines}: ',
-                style: const TextStyle(
-                  fontWeight: FontWeight.normal,
-                ),
-                children: [
-                  TextSpan(
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                    text: prefs.numEvalLines.toString(),
-                  ),
-                ],
-              ),
-            ),
-            subtitle: NonLinearSlider(
-              value: prefs.numEvalLines,
-              values: const [1, 2, 3],
-              onChangeEnd: state.isEngineAvailable
-                  ? (value) => ref
-                      .read(ctrlProvider.notifier)
-                      .setNumEvalLines(value.toInt())
-                  : null,
-            ),
-          ),
-          if (maxEngineCores > 1)
-            PlatformListTile(
-              title: Text.rich(
-                TextSpan(
-                  text: '${context.l10n.cpus}: ',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.normal,
-                  ),
-                  children: [
-                    TextSpan(
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                      text: prefs.numEngineCores.toString(),
-                    ),
-                  ],
-                ),
-              ),
-              subtitle: NonLinearSlider(
-                value: prefs.numEngineCores,
-                values: List.generate(maxEngineCores, (index) => index + 1),
-                onChangeEnd: state.isEngineAvailable
-                    ? (value) => ref
-                        .read(ctrlProvider.notifier)
-                        .setEngineCores(value.toInt())
-                    : null,
-              ),
-            ),
-          SwitchSettingTile(
-            title: Text(context.l10n.bestMoveArrow),
-            value: prefs.showBestMoveArrow,
-            onChanged: state.isEngineAvailable
-                ? (value) => ref
-                    .read(analysisPreferencesProvider.notifier)
-                    .toggleShowBestMoveArrow()
-                : null,
-          ),
-          SwitchSettingTile(
-            title: Text(context.l10n.evaluationGauge),
-            value: prefs.showEvaluationGauge,
-            onChanged: state.isEngineAvailable
-                ? (value) => ref
-                    .read(analysisPreferencesProvider.notifier)
-                    .toggleShowEvaluationGauge()
-                : null,
-          ),
-          SwitchSettingTile(
-            title: Text(context.l10n.sound),
-            value: isSoundEnabled,
-            onChanged: (value) {
-              ref
-                  .read(generalPreferencesProvider.notifier)
-                  .toggleSoundEnabled();
-            },
-          ),
-        ],
-      ),
     );
   }
 }
