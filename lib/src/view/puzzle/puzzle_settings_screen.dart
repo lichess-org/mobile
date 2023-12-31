b98113729dca3b0bccf5ef561272d56208114424
K. P. Krasiński-Sroka <kpsroka@users.noreply.github.com>
2023-12-31 21:56:06 +0100

    Adds Android-specific Scaffold in PuzzleSettingsScreen

--- lib/src/view/puzzle/puzzle_settings_screen.dart
+++ lib/src/view/puzzle/puzzle_settings_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/model/common/id.dart';
 import 'package:lichess_mobile/src/model/puzzle/puzzle_preferences.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
 import 'package:lichess_mobile/src/widgets/list.dart';
+import 'package:lichess_mobile/src/widgets/platform.dart';
 import 'package:lichess_mobile/src/widgets/settings.dart';
 
 class PuzzleSettingsScreen extends StatelessWidget {
@@ -13,8 +15,24 @@ class PuzzleSettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Puzzle settings')),
+      body: _Body(userId: userId),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(),
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Puzzle settings'),
+      ),
       child: _Body(userId: userId),
     );
   }
