655da788fc40424a1ccfe38c17cf68b562c65eff
Vincent Velociter <vincent.velociter@gmail.com>
2024-01-22 16:21:40 +0100

    Expose analysis event in server analysis service

--- lib/src/model/analysis/server_analysis_service.dart
+++ lib/src/model/analysis/server_analysis_service.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:lichess_mobile/src/model/common/id.dart';
 import 'package:lichess_mobile/src/model/common/socket.dart';
 import 'package:lichess_mobile/src/model/game/game_repository_providers.dart';
+import 'package:lichess_mobile/src/model/game/game_socket_events.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'server_analysis_service.g.dart';
@@ -19,6 +21,12 @@ class ServerAnalysisService {
 
   final ServerAnalysisServiceRef ref;
 
+  final _analysisProgress = ValueNotifier<(GameId, ServerEvalEvent)?>(null);
+
+  /// The last analysis progress event received from the server.
+  ValueListenable<(GameId, ServerEvalEvent)?> get lastAnalysisEvent =>
+      _analysisProgress;
+
   /// Request server analysis for a game.
   ///
   /// This will return a future that completes when the server analysis is
@@ -28,21 +36,31 @@ class ServerAnalysisService {
       throw Exception('You can only request analysis for one game at a time');
     }
 
+    _analysisProgress.value = null;
+
     final socket = ref.read(socketClientProvider);
     final (stream, _) = socket.connect(Uri(path: '/play/$id/v6'));
     final repo = ref.read(gameRepositoryProvider);
     final completer = Completer<void>();
+    final gameId = id.gameId;
 
     _socketSubscription = stream.listen((event) {
       // complete on first analysisProgress event
       if (event.topic == 'analysisProgress') {
         completer.complete();
-        _socketSubscription?.cancel();
-        _socketSubscription = null;
+        final data =
+            ServerEvalEvent.fromJson(event.data as Map<String, dynamic>);
+
+        _analysisProgress.value = (gameId, data);
+
+        if (!data.isAnalysisIncomplete) {
+          _socketSubscription?.cancel();
+          _socketSubscription = null;
+        }
       }
     });
 
-    final result = await repo.requestServerAnalysis(id.gameId);
+    final result = await repo.requestServerAnalysis(gameId);
 
     if (result.isError) {
       _socketSubscription?.cancel();
