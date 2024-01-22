e39f48f2dc1c3f3c8c97cff9bea21bc730ca13a3
Vincent Velociter <vincent.velociter@gmail.com>
2024-01-22 16:55:16 +0100

    Tweak analysis service

--- lib/src/model/analysis/server_analysis_service.dart
+++ lib/src/model/analysis/server_analysis_service.dart
@@ -55,7 +55,7 @@ class ServerAnalysisService {
 
         _analysisProgress.value = (gameId, data);
 
-        if (!data.isAnalysisIncomplete) {
+        if (data.isAnalysisIncomplete == false) {
           _socketSubscription?.cancel();
           _socketSubscription = null;
         }
