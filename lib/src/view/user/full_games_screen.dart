1ff450d6372866ac449d80b4a619a7a0b46a9412
ZTL-UwU <zhangtianli2006@163.com>
2024-04-15 12:46:49 +0800

    use flutter_slidable use default ios navigation bar previousPage remove wrong comment

--- lib/src/view/user/full_games_screen.dart
+++ lib/src/view/user/full_games_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:lichess_mobile/src/constants.dart';
 import 'package:lichess_mobile/src/model/account/account_repository.dart';
 import 'package:lichess_mobile/src/model/auth/auth_session.dart';
@@ -16,7 +18,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'full_games_screen.g.dart';
 
-/// Create a Screen with Top 10 players for each Lichess Variant
 class FullGameScreen extends StatelessWidget {
   const FullGameScreen({this.user, super.key});
   final LightUser? user;
@@ -29,7 +30,6 @@ class FullGameScreen extends StatelessWidget {
   Widget _buildIos(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
-        previousPageTitle: 'Home',
         middle: Text('Full Game History'),
       ),
       child: _Body(user: user),
@@ -148,13 +148,17 @@ class _GameListState extends ConsumerState<_GameList> {
                 return kEmptyWidget;
               }
             } else {
-              return _SlideMenu(
-                menuItems: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.bookmark_add_outlined),
-                    onPressed: () {},
-                  ),
-                ],
+              return Slidable(
+                endActionPane: const ActionPane(
+                  motion: ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: null,
+                      icon: Icons.bookmark_add_outlined,
+                      label: 'Bookmark',
+                    ),
+                  ],
+                ),
                 child: ExtendedGameListTile(
                   game: games[index],
                   userId: userId,
@@ -188,90 +192,5 @@ class _GameListState extends ConsumerState<_GameList> {
       });
       isLoading = false;
     }
-  }
-}
-
-class _SlideMenu extends StatefulWidget {
-  const _SlideMenu({required this.child, required this.menuItems});
-  final Widget child;
-  final List<Widget> menuItems;
-
-  @override
-  _SlideMenuState createState() => _SlideMenuState();
-}
-
-class _SlideMenuState extends State<_SlideMenu>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 200),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final animation = Tween(begin: Offset.zero, end: const Offset(-0.15, 0.0))
-        .animate(CurveTween(curve: Curves.decelerate).animate(_controller));
-
-    return GestureDetector(
-      onHorizontalDragUpdate: (data) {
-        setState(() {
-          _controller.value -= data.primaryDelta! / context.size!.width * 1.5;
-        });
-      },
-      onHorizontalDragEnd: (data) {
-        if (data.primaryVelocity! > 1500) {
-          _controller.animateTo(0);
-        } else if (_controller.value >= 0.3 || data.primaryVelocity! < -1500) {
-          _controller.animateTo(1.0);
-        } else {
-          _controller.animateTo(0);
-        }
-      },
-      child: Stack(
-        children: <Widget>[
-          SlideTransition(position: animation, child: widget.child),
-          Positioned.fill(
-            child: LayoutBuilder(
-              builder: (context, constraint) {
-                return AnimatedBuilder(
-                  animation: _controller,
-                  builder: (context, child) {
-                    return Stack(
-                      children: <Widget>[
-                        Positioned(
-                          right: 0.1,
-                          top: 0,
-                          bottom: 0,
-                          width: constraint.maxWidth * animation.value.dx * -1,
-                          child: Row(
-                            children: widget.menuItems.map((child) {
-                              return Expanded(
-                                child: child,
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
