1887dbc96d51049b887f61efae60d385a2bcc5e3
ZTL-UwU <zhangtianli2006@163.com>
2024-04-15 12:52:29 +0800

    remove unnecessary import
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- lib/src/view/user/full_games_screen.dart
+++ lib/src/view/user/full_games_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:lichess_mobile/src/constants.dart';
