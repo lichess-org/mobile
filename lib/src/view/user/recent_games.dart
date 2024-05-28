6a72d1c0b5481bb8ed20fdc76d0cdc81e2c0cef0
ZTL-UwU <zhangtianli2006@163.com>
2024-05-28 22:39:16 +0800

    Remove unused imports
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- lib/src/view/user/recent_games.dart
+++ lib/src/view/user/recent_games.dart
@@ -4,8 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/model/account/account_repository.dart';
 import 'package:lichess_mobile/src/model/auth/auth_session.dart';
-import 'package:lichess_mobile/src/model/common/id.dart';
-import 'package:lichess_mobile/src/model/game/archived_game.dart';
 import 'package:lichess_mobile/src/model/game/game_repository_providers.dart';
 import 'package:lichess_mobile/src/model/game/game_storage.dart';
 import 'package:lichess_mobile/src/model/user/user.dart';
