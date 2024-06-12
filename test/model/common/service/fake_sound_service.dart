e0d8437d425d98d60ff36ce9d6a17991253075aa
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-12 20:15:41 +0200

    test: override new fields in FakeSoundService

--- test/model/common/service/fake_sound_service.dart
+++ test/model/common/service/fake_sound_service.dart
@@ -3,9 +3,15 @@ import 'package:lichess_mobile/src/model/settings/sound_theme.dart';
 
 class FakeSoundService implements SoundService {
   @override
+  final double masterVolume = 0;
+
+  @override
   Future<int?> play(Sound sound) async {
     return null;
   }
+
+  @override
+  Future<void> setVolume() async {}
 
   @override
   Future<void> stopCurrent() async {}
