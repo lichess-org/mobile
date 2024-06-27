0ae3d894a34666a49fae616ac38c531d5468a38a
Vincent Velociter <vincent.velociter@gmail.com>
2024-06-27 12:13:10 +0200

    Direct access to analysis screen from tools

--- lib/src/view/tools/tools_tab_screen.dart
+++ lib/src/view/tools/tools_tab_screen.dart
@@ -1,11 +1,15 @@
+import 'package:dartchess/dartchess.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lichess_mobile/src/model/analysis/analysis_controller.dart';
+import 'package:lichess_mobile/src/model/common/chess.dart';
 import 'package:lichess_mobile/src/navigation.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
 import 'package:lichess_mobile/src/utils/navigation.dart';
 import 'package:lichess_mobile/src/view/analysis/analysis_position_choice_screen.dart';
+import 'package:lichess_mobile/src/view/analysis/analysis_screen.dart';
 import 'package:lichess_mobile/src/view/clock/clock_screen.dart';
 import 'package:lichess_mobile/src/widgets/list.dart';
 import 'package:lichess_mobile/src/widgets/platform.dart';
@@ -69,28 +73,65 @@ class _Body extends StatelessWidget {
       ListSection(
         hasLeading: true,
         children: [
-          PlatformListTile(
-            leading: Icon(
-              Icons.biotech,
-              size: Styles.mainListTileIconSize,
-              color: Theme.of(context).platform == TargetPlatform.iOS
-                  ? CupertinoTheme.of(context).primaryColor
-                  : Theme.of(context).colorScheme.primary,
-            ),
-            title: Padding(
-              padding: tilePadding,
-              child: Text(context.l10n.analysis, style: Styles.callout),
-            ),
-            trailing: Theme.of(context).platform == TargetPlatform.iOS
-                ? const CupertinoListTileChevron()
-                : null,
-            onTap: () => pushPlatformRoute(
-              context,
-              builder: (context) => const AnalysisPositionChoiceScreen(),
+          Padding(
+            padding: Theme.of(context).platform == TargetPlatform.android
+                ? const EdgeInsets.only(bottom: 16.0)
+                : EdgeInsets.zero,
+            child: PlatformListTile(
+              leading: Icon(
+                Icons.biotech,
+                size: Styles.mainListTileIconSize,
+                color: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoTheme.of(context).primaryColor
+                    : Theme.of(context).colorScheme.primary,
+              ),
+              title: Padding(
+                padding: tilePadding,
+                child: Text(context.l10n.analysis, style: Styles.callout),
+              ),
+              trailing: Theme.of(context).platform == TargetPlatform.iOS
+                  ? const CupertinoListTileChevron()
+                  : null,
+              onTap: () => pushPlatformRoute(
+                context,
+                rootNavigator: true,
+                builder: (context) => const AnalysisScreen(
+                  pgnOrId: '',
+                  options: AnalysisOptions(
+                    isLocalEvaluationAllowed: true,
+                    variant: Variant.standard,
+                    orientation: Side.white,
+                    id: standaloneAnalysisId,
+                  ),
+                ),
+              ),
             ),
           ),
-          if (Theme.of(context).platform == TargetPlatform.android)
-            const SizedBox(height: 16.0),
+          Padding(
+            padding: Theme.of(context).platform == TargetPlatform.android
+                ? const EdgeInsets.only(bottom: 16.0)
+                : EdgeInsets.zero,
+            child: PlatformListTile(
+              leading: Icon(
+                Icons.upload_file,
+                size: Styles.mainListTileIconSize,
+                color: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoTheme.of(context).primaryColor
+                    : Theme.of(context).colorScheme.primary,
+              ),
+              title: Padding(
+                padding: tilePadding,
+                child: Text(context.l10n.loadPosition, style: Styles.callout),
+              ),
+              trailing: Theme.of(context).platform == TargetPlatform.iOS
+                  ? const CupertinoListTileChevron()
+                  : null,
+              onTap: () => pushPlatformRoute(
+                context,
+                builder: (context) => const AnalysisPositionChoiceScreen(),
+              ),
+            ),
+          ),
           PlatformListTile(
             leading: Icon(
               Icons.alarm,
