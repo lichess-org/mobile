112b5d76897f6a01cbc19076682a8d8e79a59922
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-07-05 21:47:47 +0100

    added trailing comma

--- test/model/common/time_increment_test.dart
+++ test/model/common/time_increment_test.dart
@@ -30,8 +30,10 @@ void main() {
     });
 
     test('Estimated Duration', () {
-      expect(const TimeIncrement(300, 5).estimatedDuration,
-          const Duration(seconds: 300 + 5 * 40));
+      expect(
+        const TimeIncrement(300, 5).estimatedDuration,
+        const Duration(seconds: 300 + 5 * 40),
+      );
       expect(const TimeIncrement(0, 0).estimatedDuration, Duration.zero);
     });
 
