60c8d2adc6a7d4203ab940e53e9929e1b7a2fd19
Vincent Velociter <vincent.velociter@gmail.com>
2024-09-03 10:02:54 +0200

    Expand bottom bar children

--- lib/src/widgets/bottom_bar.dart
+++ lib/src/widgets/bottom_bar.dart
@@ -9,6 +9,7 @@ class BottomBar extends StatelessWidget {
   const BottomBar({
     required this.children,
     this.mainAxisAlignment = MainAxisAlignment.spaceAround,
+    this.expandChildren = true,
   });
 
   /// Children to display in the bottom bar's [Row]. Typically instances of [BottomBarButton].
@@ -16,6 +17,9 @@ class BottomBar extends StatelessWidget {
 
   /// Alignment of the bottom bar's internal row. Defaults to [MainAxisAlignment.spaceAround].
   final MainAxisAlignment mainAxisAlignment;
+
+  /// Whether to expand the children to fill the available space. Defaults to true.
+  final bool expandChildren;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +33,9 @@ class BottomBar extends StatelessWidget {
           height: kBottomBarHeight,
           child: Row(
             mainAxisAlignment: mainAxisAlignment,
-            children: children,
+            children: expandChildren
+                ? children.map((child) => Expanded(child: child)).toList()
+                : children,
           ),
         ),
       ),
