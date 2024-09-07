d5192d17cde03cecaa7803cc701788cc02e23ecb
Vincent Velociter <vincent.velociter@gmail.com>
2024-09-07 11:23:35 +0200

    Fix loading games from explorer
    Fixes #984

--- test/view/game/archived_game_screen_test.dart
+++ test/view/game/archived_game_screen_test.dart
@@ -30,6 +30,39 @@ final client = MockClient((request) {
 void main() {
   group('ArchivedGameScreen', () {
     testWidgets(
+      'loads game data if only game id is provided',
+      (tester) async {
+        final app = await buildTestApp(
+          tester,
+          home: const ArchivedGameScreen(
+            gameId: GameId('qVChCOTc'),
+            orientation: Side.white,
+          ),
+          overrides: [
+            lichessClientProvider
+                .overrideWith((ref) => LichessClient(client, ref)),
+          ],
+        );
+
+        await tester.pumpWidget(app);
+
+        expect(find.byType(PieceWidget), findsNothing);
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+        // wait for game data loading
+        await tester.pump(const Duration(milliseconds: 100));
+
+        expect(find.byType(PieceWidget), findsNWidgets(25));
+        expect(find.widgetWithText(GamePlayer, 'veloce'), findsOneWidget);
+        expect(
+          find.widgetWithText(GamePlayer, 'Stockfish level 1'),
+          findsOneWidget,
+        );
+      },
+      variant: kPlatformVariant,
+    );
+
+    testWidgets(
       'displays game data and last fen immediately, then moves',
       (tester) async {
         final app = await buildTestApp(
@@ -188,7 +221,7 @@ void main() {
 // --
 
 const gameResponse = '''
-{"id":"qVChCOTc","rated":false,"variant":"standard","speed":"blitz","perf":"blitz","createdAt":1673443822389,"lastMoveAt":1673444036416,"status":"mate","players":{"white":{"aiLevel":1},"black":{"user":{"name":"veloce","patron":true,"id":"veloce"},"rating":1435,"provisional":true}},"winner":"black","opening":{"eco":"C20","name":"King's Pawn Game: Wayward Queen Attack, Kiddie Countergambit","ply":4},"moves":"e4 e5 Qh5 Nf6 Qxe5+ Be7 b3 d6 Qb5+ Bd7 Qxb7 Nc6 Ba3 Rb8 Qa6 Nxe4 Bb2 O-O Nc3 Nb4 Nf3 Nxa6 Nd5 Nb4 Nxe7+ Qxe7 Nd4 Qf6 f4 Qe7 Ke2 Ng3+ Kd1 Nxh1 Bc4 Nf2+ Kc1 Qe1#","clocks":[18003,18003,17915,17627,17771,16691,17667,16243,17475,15459,17355,14779,17155,13795,16915,13267,14771,11955,14451,10995,14339,10203,13899,9099,12427,8379,12003,7547,11787,6691,11355,6091,11147,5763,10851,5099,10635,4657],"clock":{"initial":180,"increment":0,"totalTime":180}}
+{"id":"qVChCOTc","rated":false,"variant":"standard","speed":"blitz","perf":"blitz","createdAt":1673443822389,"lastMoveAt":1673444036416,"status":"mate","players":{"white":{"aiLevel":1},"black":{"user":{"name":"veloce","patron":true,"id":"veloce"},"rating":1435,"provisional":true}},"winner":"black","opening":{"eco":"C20","name":"King's Pawn Game: Wayward Queen Attack, Kiddie Countergambit","ply":4},"moves":"e4 e5 Qh5 Nf6 Qxe5+ Be7 b3 d6 Qb5+ Bd7 Qxb7 Nc6 Ba3 Rb8 Qa6 Nxe4 Bb2 O-O Nc3 Nb4 Nf3 Nxa6 Nd5 Nb4 Nxe7+ Qxe7 Nd4 Qf6 f4 Qe7 Ke2 Ng3+ Kd1 Nxh1 Bc4 Nf2+ Kc1 Qe1#","clocks":[18003,18003,17915,17627,17771,16691,17667,16243,17475,15459,17355,14779,17155,13795,16915,13267,14771,11955,14451,10995,14339,10203,13899,9099,12427,8379,12003,7547,11787,6691,11355,6091,11147,5763,10851,5099,10635,4657],"clock":{"initial":180,"increment":0,"totalTime":180},"lastFen":"1r3rk1/p1pb1ppp/3p4/8/1nBN1P2/1P6/PBPP1nPP/R1K1q3 w - - 4 1"}
 ''';
 
 final gameData = LightArchivedGame(
