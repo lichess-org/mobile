d30d5a80e1842c56f3418b9dcddc1784de2f6455
Julien <120588494+julien4215@users.noreply.github.com>
2024-09-17 20:07:08 +0200

    revert format change to avoid conflict

--- lib/src/view/broadcast/broadcasts_list_screen.dart
+++ lib/src/view/broadcast/broadcasts_list_screen.dart
@@ -198,103 +198,104 @@ class BroadcastGridItem extends StatelessWidget {
         );
 
   @override
-  Widget build(BuildContext context) => AdaptiveInkWell(
-        borderRadius: BorderRadius.circular(20),
-        onTap: () {
-          pushPlatformRoute(
-            context,
-            title: context.l10n.broadcastBroadcasts,
-            rootNavigator: true,
-            builder: (context) => BroadcastScreen(broadcast: broadcast),
-          );
-        },
-        child: Container(
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: LichessColors.grey.withOpacity(0.5),
-                blurRadius: 5,
-                spreadRadius: 1,
-              ),
-            ],
-          ),
-          foregroundDecoration: BoxDecoration(
-            border: (broadcast.isLive)
-                ? Border.all(color: LichessColors.red, width: 2)
-                : Border.all(color: LichessColors.grey),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            children: [
-              if (broadcast.tour.imageUrl != null)
-                AspectRatio(
-                  aspectRatio: 2.0,
-                  child: FadeInImage.memoryNetwork(
-                    placeholder: transparentImage,
-                    image: broadcast.tour.imageUrl!,
-                  ),
-                )
-              else
-                const DefaultBroadcastImage(aspectRatio: 2.0),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          if (!broadcast.isFinished) ...[
-                            Text(
-                              broadcast.round.name,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelMedium
-                                  ?.copyWith(
-                                    color: textShade(context, 0.5),
-                                  ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(width: 4.0),
-                          ],
-                          if (broadcast.isLive)
-                            const Text(
-                              'LIVE',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            )
-                          else if (broadcast.round.startsAt != null)
-                            StartsRoundDate(
-                              startsAt: broadcast.round.startsAt!,
-                            ),
+  Widget build(BuildContext context) {
+    return AdaptiveInkWell(
+      borderRadius: BorderRadius.circular(20),
+      onTap: () {
+        pushPlatformRoute(
+          context,
+          title: context.l10n.broadcastBroadcasts,
+          rootNavigator: true,
+          builder: (context) => BroadcastScreen(broadcast: broadcast),
+        );
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: LichessColors.grey.withOpacity(0.5),
+              blurRadius: 5,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        foregroundDecoration: BoxDecoration(
+          border: (broadcast.isLive)
+              ? Border.all(color: LichessColors.red, width: 2)
+              : Border.all(color: LichessColors.grey),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            if (broadcast.tour.imageUrl != null)
+              AspectRatio(
+                aspectRatio: 2.0,
+                child: FadeInImage.memoryNetwork(
+                  placeholder: transparentImage,
+                  image: broadcast.tour.imageUrl!,
+                ),
+              )
+            else
+              const DefaultBroadcastImage(aspectRatio: 2.0),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        if (!broadcast.isFinished) ...[
+                          Text(
+                            broadcast.round.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium
+                                ?.copyWith(
+                                  color: textShade(context, 0.5),
+                                ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(width: 4.0),
                         ],
+                        if (broadcast.isLive)
+                          const Text(
+                            'LIVE',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          )
+                        else if (broadcast.round.startsAt != null)
+                          StartsRoundDate(
+                            startsAt: broadcast.round.startsAt!,
+                          ),
+                      ],
+                    ),
+                    const SizedBox(height: 4.0),
+                    Flexible(
+                      child: Text(
+                        broadcast.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
                       ),
-                      const SizedBox(height: 4.0),
-                      Flexible(
-                        child: Text(
-                          broadcast.title,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style:
-                              Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
 
 class StartsRoundDate extends ConsumerWidget {
