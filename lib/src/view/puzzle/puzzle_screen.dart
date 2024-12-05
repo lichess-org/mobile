ef64fb5841d27ba01d11e0586371b815d40e859a
Matthew Dang <dangmatthewdang@gmail.com>
2024-12-04 20:58:43 -0500

    Added hint button, need to add functionality.

--- lib/src/view/puzzle/puzzle_screen.dart
+++ lib/src/view/puzzle/puzzle_screen.dart
@@ -391,6 +391,15 @@ class _BottomBar extends ConsumerWidget {
         if (puzzleState.mode != PuzzleMode.view)
           BottomBarButton(
             icon: Icons.help,
+            label: context.l10n.getAHint,
+            showLabel: true,
+            onTap: puzzleState.canViewSolution
+                ? () => ref.read(ctrlProvider.notifier).viewSolution()
+                : null,
+          ),
+        if (puzzleState.mode != PuzzleMode.view)
+          BottomBarButton(
+            icon: Icons.help,
             label: context.l10n.viewTheSolution,
             showLabel: true,
             onTap: puzzleState.canViewSolution
