052ffcd6c63756976391e94038264a4e17efeb49
Matthew Dang <dangmatthewdang@gmail.com>
2024-12-04 22:34:00 -0500

    Switch hint icon from help to info to distinguish from view the solution.

--- lib/src/view/puzzle/puzzle_screen.dart
+++ lib/src/view/puzzle/puzzle_screen.dart
@@ -390,7 +390,7 @@ class _BottomBar extends ConsumerWidget {
           ),
         if (puzzleState.mode != PuzzleMode.view)
           BottomBarButton(
-            icon: Icons.help,
+            icon: Icons.info,
             label: context.l10n.getAHint,
             showLabel: true,
             onTap: puzzleState.canViewSolution
