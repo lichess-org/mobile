5e820e7dcf3667dfb5643e17630acf259f29bd28
Matthew Dang <dangmatthewdang@gmail.com>
2024-12-12 17:38:46 -0500

    1 more trailing , and format

--- lib/src/view/puzzle/puzzle_screen.dart
+++ lib/src/view/puzzle/puzzle_screen.dart
@@ -295,10 +295,16 @@ class _Body extends ConsumerWidget {
                             color: const Color(0x40003088),
                             orig: puzzleState.hintMove!.from,
                           ),
-                        ]).addAll(puzzleState.hintPossibleMoves!
-                          .map((i) =>
-                              Circle(color: const Color(0x40003088), orig: i),)
-                          .toList(),)
+                        ]).addAll(
+                          puzzleState.hintPossibleMoves!
+                              .map(
+                                (i) => Circle(
+                                  color: const Color(0x40003088),
+                                  orig: i,
+                                ),
+                              )
+                              .toList(),
+                        )
                       : null,
               engineGauge: puzzleState.isEngineEnabled
                   ? (
