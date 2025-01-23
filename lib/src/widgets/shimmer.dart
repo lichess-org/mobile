ffefc06500a1c9a9457ea0ac0a96d1da7a87dd72
Vincent Velociter <vincent.velociter@gmail.com>
2025-01-23 18:46:46 +0800

    Color fixes

--- lib/src/widgets/shimmer.dart
+++ lib/src/widgets/shimmer.dart
@@ -37,9 +37,9 @@ class ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
       case Brightness.dark:
         return LinearGradient(
           colors: [
+            lighten(scaffoldBackgroundColor, 0.05),
             lighten(scaffoldBackgroundColor, 0.1),
             lighten(scaffoldBackgroundColor, 0.2),
-            lighten(scaffoldBackgroundColor, 0.3),
           ],
           stops: const [0.1, 0.3, 0.4],
           begin: const Alignment(-1.0, -0.3),
