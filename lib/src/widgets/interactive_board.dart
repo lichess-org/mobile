b6960c1a4a4315d8a4f7b588d11988cb80d4f27b
Jimima <jimmy_james@fastmail.co.uk>
2025-02-13 15:03:04 +0000

    Removed import

--- lib/src/widgets/interactive_board.dart
+++ lib/src/widgets/interactive_board.dart
@@ -1,7 +1,6 @@
 import 'package:chessground/chessground.dart';
 import 'package:dartchess/dartchess.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lichess_mobile/src/model/settings/board_preferences.dart';
 
