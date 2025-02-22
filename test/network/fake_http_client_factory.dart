bcbb88700a7b6bc3088cfa07f33c91f113898107
Vincenzo De Petris <vincenzo.depetris@eng.it>
2025-02-22 23:02:44 +0100

    run dart format

--- test/network/fake_http_client_factory.dart
+++ test/network/fake_http_client_factory.dart
@@ -5,7 +5,7 @@ class FakeHttpClientFactory implements HttpClientFactory {
   const FakeHttpClientFactory(this._factory);
 
   final http.Client Function() _factory;
-  
+
   @override
   http.Client Function(http.Client client)? get onCreate => throw UnimplementedError();
 
