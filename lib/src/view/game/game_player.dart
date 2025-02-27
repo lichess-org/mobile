934225ea536bd24976f1b8bc93ae0ef96a2dba57
Matthew Dang <dangmatthewdang@gmail.com>
2025-02-27 00:13:07 -0500

    Formatted.

--- lib/src/view/game/game_player.dart
+++ lib/src/view/game/game_player.dart
@@ -135,9 +135,10 @@ class GamePlayer extends StatelessWidget {
                           TextSpan(
                             text: ' ${player.ratingDiff! > 0 ? '+' : ''}${player.ratingDiff}',
                             style: TextStyle(
-                              color: player.ratingDiff! > 0
-                                  ? context.lichessColors.good
-                                  : player.ratingDiff! == 0
+                              color:
+                                  player.ratingDiff! > 0
+                                      ? context.lichessColors.good
+                                      : player.ratingDiff! == 0
                                       ? context.lichessColors.brag
                                       : context.lichessColors.error,
                             ),
