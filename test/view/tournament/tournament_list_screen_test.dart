febb10ccde1edc211ab1321e9717672a345f2b7e
tom-anders <13141438+tom-anders@users.noreply.github.com>
2025-03-10 09:27:24 +0100

    cleanup imports

--- test/view/tournament/tournament_list_screen_test.dart
+++ test/view/tournament/tournament_list_screen_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/testing.dart';
 import 'package:lichess_mobile/src/network/http.dart';
