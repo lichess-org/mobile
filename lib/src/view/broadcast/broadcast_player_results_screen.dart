lichess-org/mobile

commit b50bcae5dd70c3dd732398b8b3a6589f26a3decb

More composition of broadcast players classes and a lot of renaming of broadcast players classes with some refactoring of BroadcastPlayerWidget

lib/src/view/broadcast/broadcast_player_results_screen.dart
@@ -20,32 +20,18 @@ import 'package:lichess_mobile/src/widgets/stat_card.dart';
 
 class BroadcastPlayerResultsScreenLoading extends ConsumerWidget {
   final BroadcastRoundId roundId;
-  final String playerId;
-  final String? playerTitle;
-  final String playerName;
+  final BroadcastPlayer player;
 
-  const BroadcastPlayerResultsScreenLoading(
-    this.roundId,
-    this.playerId, {
-    required this.playerName,
-    this.playerTitle,
-  });
+  const BroadcastPlayerResultsScreenLoading({required this.roundId, required this.player});
 
   static Route<dynamic> buildRoute(
     BuildContext context,
     BroadcastRoundId roundId,
-    String playerId, {
-    String? playerTitle,
-    required String playerName,
-  }) {
+    BroadcastPlayer player,
+  ) {
     return buildScreenRoute(
       context,
-      screen: BroadcastPlayerResultsScreenLoading(
-        roundId,
-        playerId,
-        playerTitle: playerTitle,
-        playerName: playerName,
-      ),
+      screen: BroadcastPlayerResultsScreenLoading(roundId: roundId, player: player),
     );
   }
 
@@ -54,11 +40,9 @@ class BroadcastPlayerResultsScreenLoading extends ConsumerWidget {
     final tournamentId = ref.watch(broadcastTournamentIdProvider(roundId));
 
     return switch (tournamentId) {
-      AsyncData(:final value) => BroadcastPlayerResultsScreen(
-        value,
-        playerId,
-        playerTitle: playerTitle,
-        playerName: playerName,
+      AsyncData(value: final tournamentId) => BroadcastPlayerResultsScreen(
+        tournamentId: tournamentId,
+        player: player,
       ),
       AsyncError(:final error) => PlatformScaffold(
         appBarTitle: const Text(''),
@@ -74,40 +58,26 @@ class BroadcastPlayerResultsScreenLoading extends ConsumerWidget {
 
 class BroadcastPlayerResultsScreen extends StatelessWidget {
   final BroadcastTournamentId tournamentId;
-  final String playerId;
-  final String? playerTitle;
-  final String playerName;
+  final BroadcastPlayer player;
 
-  const BroadcastPlayerResultsScreen(
-    this.tournamentId,
-    this.playerId, {
-    required this.playerName,
-    this.playerTitle,
-  });
+  const BroadcastPlayerResultsScreen({required this.tournamentId, required this.player});
 
   static Route<dynamic> buildRoute(
     BuildContext context,
     BroadcastTournamentId tournamentId,
-    String playerId, {
-    String? playerTitle,
-    required String playerName,
-  }) {
+    BroadcastPlayer player,
+  ) {
     return buildScreenRoute(
       context,
-      screen: BroadcastPlayerResultsScreen(
-        tournamentId,
-        playerId,
-        playerTitle: playerTitle,
-        playerName: playerName,
-      ),
+      screen: BroadcastPlayerResultsScreen(tournamentId: tournamentId, player: player),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
-      appBarTitle: BroadcastPlayerWidget(title: playerTitle, name: playerName),
-      body: _Body(tournamentId, playerId),
+      appBarTitle: BroadcastPlayerWidget(player: player, showFederation: false, showRating: false),
+      body: _Body(tournamentId, player.id),
     );
   }
 }
@@ -122,20 +92,30 @@ class _Body extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final playersResults = ref.watch(broadcastPlayerResultProvider(tournamentId, playerId));
+    final playerWithGameResults = ref.watch(broadcastPlayerProvider(tournamentId, playerId));
 
-    switch (playersResults) {
-      case AsyncData(value: final playerResults):
-        final player = playerResults.player;
-        final fideData = playerResults.fideData;
-        final showRatingDiff = playerResults.games.any((result) => result.ratingDiff != null);
+    switch (playerWithGameResults) {
+      case AsyncData(value: final playerWithGameResults):
+        final playerWithOverallResult = playerWithGameResults.player;
+        final player = playerWithOverallResult.player;
+        final score = playerWithOverallResult.score;
+        final played = playerWithOverallResult.played;
+        final performance = playerWithOverallResult.performance;
+        final ratingDiff = playerWithOverallResult.ratingDiff;
+        final fideData = playerWithGameResults.fideData;
+        final showRatingDiff = playerWithGameResults.games.any(
+          (result) => result.ratingDiff != null,
+        );
         final statWidth =
             (MediaQuery.sizeOf(context).width - Styles.bodyPadding.horizontal - 10 * 2) / 3;
         const cardSpacing = 10.0;
-        final indexWidth = max(8.0 + playerResults.games.length.toString().length * 10.0, 28.0);
+        final indexWidth = max(
+          8.0 + playerWithGameResults.games.length.toString().length * 10.0,
+          28.0,
+        );
 
         return ListView.builder(
-          itemCount: playerResults.games.length + 1,
+          itemCount: playerWithGameResults.games.length + 1,
           itemBuilder: (context, index) {
             if (index == 0) {
               return Padding(
@@ -226,29 +206,29 @@ class _Body extends ConsumerWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       spacing: cardSpacing,
                       children: [
-                        if (player.score != null)
+                        if (score != null)
                           SizedBox(
                             width: statWidth,
                             child: _StatCard(
                               context.l10n.broadcastScore,
                               value:
-                                  '${player.score!.toStringAsFixed((player.score! == player.score!.roundToDouble()) ? 0 : 1)} / ${player.played}',
+                                  '${score.toStringAsFixed((score == score.roundToDouble()) ? 0 : 1)} / $played',
                             ),
                           ),
-                        if (player.performance != null)
+                        if (performance != null)
                           SizedBox(
                             width: statWidth,
                             child: _StatCard(
                               context.l10n.performance,
-                              value: player.performance.toString(),
+                              value: performance.toString(),
                             ),
                           ),
-                        if (player.ratingDiff != null)
+                        if (ratingDiff != null)
                           SizedBox(
                             width: statWidth,
                             child: _StatCard(
                               context.l10n.broadcastRatingDiff,
-                              child: ProgressionWidget(player.ratingDiff!, fontSize: 18.0),
+                              child: ProgressionWidget(ratingDiff, fontSize: 18.0),
                             ),
                           ),
                       ],
@@ -258,7 +238,7 @@ class _Body extends ConsumerWidget {
               );
             }
 
-            final playerResult = playerResults.games[index - 1];
+            final playerGameResult = playerWithGameResults.games[index - 1];
 
             return GestureDetector(
               onTap: () {
@@ -266,8 +246,8 @@ class _Body extends ConsumerWidget {
                   BroadcastGameScreen.buildRoute(
                     context,
                     tournamentId: tournamentId,
-                    roundId: playerResult.roundId,
-                    gameId: playerResult.gameId,
+                    roundId: playerGameResult.roundId,
+                    gameId: playerGameResult.gameId,
                   ),
                 );
               },
@@ -288,17 +268,13 @@ class _Body extends ConsumerWidget {
                       ),
                       Expanded(
                         flex: 5,
-                        child: BroadcastPlayerWidget(
-                          federation: playerResult.opponent.federation,
-                          title: playerResult.opponent.title,
-                          name: playerResult.opponent.name,
-                        ),
+                        child: BroadcastPlayerWidget(player: player, showRating: false),
                       ),
                       Expanded(
                         flex: 3,
                         child:
-                            (playerResult.opponent.rating != null)
-                                ? Center(child: Text(playerResult.opponent.rating.toString()))
+                            (playerGameResult.opponent.rating != null)
+                                ? Center(child: Text(playerGameResult.opponent.rating.toString()))
                                 : const SizedBox.shrink(),
                       ),
                       SizedBox(
@@ -310,16 +286,16 @@ class _Body extends ConsumerWidget {
                             decoration: BoxDecoration(
                               border:
                                   (Theme.of(context).brightness == Brightness.light &&
-                                              playerResult.color == Side.white ||
+                                              playerGameResult.color == Side.white ||
                                           Theme.of(context).brightness == Brightness.dark &&
-                                              playerResult.color == Side.black)
+                                              playerGameResult.color == Side.black)
                                       ? Border.all(
                                         width: 2.0,
                                         color: ColorScheme.of(context).outline,
                                       )
                                       : null,
                               shape: BoxShape.circle,
-                              color: switch (playerResult.color) {
+                              color: switch (playerGameResult.color) {
                                 Side.white => Colors.white.withValues(alpha: 0.9),
                                 Side.black => Colors.black.withValues(alpha: 0.9),
                               },
@@ -331,7 +307,7 @@ class _Body extends ConsumerWidget {
                         width: 30,
                         child: Center(
                           child: Text(
-                            switch (playerResult.points) {
+                            switch (playerGameResult.points) {
                               BroadcastPoints.one => '1',
                               BroadcastPoints.half => '½',
                               BroadcastPoints.zero => '0',
@@ -340,7 +316,7 @@ class _Body extends ConsumerWidget {
                             style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
-                              color: switch (playerResult.points) {
+                              color: switch (playerGameResult.points) {
                                 BroadcastPoints.one => context.lichessColors.good,
                                 BroadcastPoints.zero => context.lichessColors.error,
                                 _ => null,
@@ -353,8 +329,8 @@ class _Body extends ConsumerWidget {
                         SizedBox(
                           width: 38,
                           child:
-                              (playerResult.ratingDiff != null)
-                                  ? ProgressionWidget(playerResult.ratingDiff!, fontSize: 14)
+                              (playerGameResult.ratingDiff != null)
+                                  ? ProgressionWidget(playerGameResult.ratingDiff!, fontSize: 14)
                                   : null,
                         ),
                     ],

lib/src/model/broadcast/broadcast.dart
@@ -140,7 +140,7 @@ class BroadcastGame with _$BroadcastGame {
 
   const factory BroadcastGame({
     required BroadcastGameId id,
-    required IMap<Side, BroadcastPlayer> players,
+    required IMap<Side, BroadcastPlayerWithClock> players,
     required String fen,
     required Move? lastMove,
     required Duration? thinkTime,
@@ -159,52 +159,59 @@ class BroadcastGame with _$BroadcastGame {
 
 @freezed
 class BroadcastPlayer with _$BroadcastPlayer {
+  const BroadcastPlayer._();
+
   const factory BroadcastPlayer({
     required String name,
     required String? title,
     required int? rating,
-    required Duration? clock,
     required String? federation,
     required FideId? fideId,
   }) = _BroadcastPlayer;
+
+  String get id => (fideId != null) ? fideId.toString() : name;
 }
 
 @freezed
-class BroadcastPlayerExtended with _$BroadcastPlayerExtended {
-  const factory BroadcastPlayerExtended({
-    required String name,
-    required String? title,
-    required int? rating,
-    required String? federation,
-    required FideId? fideId,
+class BroadcastPlayerWithClock with _$BroadcastPlayerWithClock {
+  const factory BroadcastPlayerWithClock({
+    required BroadcastPlayer player,
+    required Duration? clock,
+  }) = _BroadcastPlayerWithClock;
+}
+
+@freezed
+class BroadcastPlayerWithOverallResult with _$BroadcastPlayerWithOverallResult {
+  const factory BroadcastPlayerWithOverallResult({
+    required BroadcastPlayer player,
     required int played,
     required double? score,
     required int? ratingDiff,
     required int? performance,
-  }) = _BroadcastPlayerExtended;
+  }) = _BroadcastPlayerWithOverallResult;
 }
 
 typedef BroadcastFideData = ({({int? standard, int? rapid, int? blitz}) ratings, int? birthYear});
 
-typedef BroadcastPlayerResults =
+typedef BroadcastPlayerWithGameResults =
     ({
-      BroadcastPlayerExtended player,
+      BroadcastPlayerWithOverallResult player,
       BroadcastFideData fideData,
-      IList<BroadcastPlayerResultData> games,
+      IList<BroadcastPlayerGameResult> games,
     });
 
 enum BroadcastPoints { one, half, zero }
 
 @freezed
-class BroadcastPlayerResultData with _$BroadcastPlayerResultData {
-  const factory BroadcastPlayerResultData({
+class BroadcastPlayerGameResult with _$BroadcastPlayerGameResult {
+  const factory BroadcastPlayerGameResult({
     required BroadcastRoundId roundId,
     required BroadcastGameId gameId,
     required Side color,
     required BroadcastPoints? points,
     required int? ratingDiff,
     required BroadcastPlayer opponent,
-  }) = _BroadcastPlayerResult;
+  }) = _BroadcastPlayerGameResult;
 }
 
 enum RoundStatus { live, finished, upcoming }

lib/src/model/broadcast/broadcast_providers.dart
@@ -58,7 +58,7 @@ Future<BroadcastRoundResponse> broadcastRound(Ref ref, BroadcastRoundId roundId)
 }
 
 @riverpod
-Future<IList<BroadcastPlayerExtended>> broadcastPlayers(
+Future<IList<BroadcastPlayerWithOverallResult>> broadcastPlayers(
   Ref ref,
   BroadcastTournamentId tournamentId,
 ) {
@@ -66,7 +66,7 @@ Future<IList<BroadcastPlayerExtended>> broadcastPlayers(
 }
 
 @riverpod
-Future<BroadcastPlayerResults> broadcastPlayerResult(
+Future<BroadcastPlayerWithGameResults> broadcastPlayer(
   Ref ref,
   BroadcastTournamentId broadcastTournamentId,
   String playerId,

lib/src/model/broadcast/broadcast_repository.dart
@@ -40,20 +40,20 @@ class BroadcastRepository {
     return client.read(Uri(path: 'api/study/$roundId/$gameId.pgn'));
   }
 
-  Future<IList<BroadcastPlayerExtended>> getPlayers(BroadcastTournamentId tournamentId) {
+  Future<IList<BroadcastPlayerWithOverallResult>> getPlayers(BroadcastTournamentId tournamentId) {
     return client.readJsonList(
       Uri(path: '/broadcast/$tournamentId/players'),
-      mapper: _makePlayerFromJson,
+      mapper: _makePlayerWithOverallResultFromJson,
     );
   }
 
-  Future<BroadcastPlayerResults> getPlayerResults(
+  Future<BroadcastPlayerWithGameResults> getPlayerResults(
     BroadcastTournamentId tournamentId,
     String playerId,
   ) {
     return client.readJson(
       Uri(path: 'broadcast/$tournamentId/players/$playerId'),
-      mapper: _makePlayerResultsFromJson,
+      mapper: _makePlayerWithGameResultsFromJson,
     );
   }
 }
@@ -171,12 +171,12 @@ MapEntry<BroadcastGameId, BroadcastGame> gameFromPick(RequiredPick pick) {
     BroadcastGame(
       id: pick('id').asBroadcastGameIdOrThrow(),
       players: IMap({
-        Side.white: _playerFromPick(
+        Side.white: _playerWithClockFromPick(
           pick('players', 0).required(),
           isPlaying: playingSide == Side.white,
           thinkTime: thinkTime,
         ),
-        Side.black: _playerFromPick(
+        Side.black: _playerWithClockFromPick(
           pick('players', 1).required(),
           isPlaying: playingSide == Side.black,
           thinkTime: thinkTime,
@@ -191,34 +191,40 @@ MapEntry<BroadcastGameId, BroadcastGame> gameFromPick(RequiredPick pick) {
   );
 }
 
-BroadcastPlayer _playerFromPick(
-  RequiredPick pick, {
-  required bool isPlaying,
-  required Duration? thinkTime,
-}) {
-  final clock = pick('clock').asDurationFromCentiSecondsOrNull();
-  final updatedClock = clock != null && isPlaying ? clock - (thinkTime ?? Duration.zero) : clock;
+BroadcastPlayer _playerFromPick(RequiredPick pick) {
   return BroadcastPlayer(
     name: pick('name').asStringOrThrow(),
     title: pick('title').asStringOrNull(),
     rating: pick('rating').asIntOrNull(),
-    clock: (updatedClock?.isNegative ?? false) ? Duration.zero : updatedClock,
     federation: pick('fed').asStringOrNull(),
     fideId: pick('fideId').asFideIdOrNull(),
   );
 }
 
-BroadcastPlayerExtended _makePlayerFromJson(Map<String, dynamic> json) {
-  return _playerExtendedFromPick(pick(json).required());
+BroadcastPlayerWithClock _playerWithClockFromPick(
+  RequiredPick pick, {
+  required bool isPlaying,
+  required Duration? thinkTime,
+}) {
+  final player = _playerFromPick(pick);
+  final clock = pick('clock').asDurationFromCentiSecondsOrNull();
+  final updatedClock = clock != null && isPlaying ? clock - (thinkTime ?? Duration.zero) : clock;
+
+  return BroadcastPlayerWithClock(
+    player: player,
+    clock: (updatedClock?.isNegative ?? false) ? Duration.zero : updatedClock,
+  );
 }
 
-BroadcastPlayerExtended _playerExtendedFromPick(RequiredPick pick) {
-  return BroadcastPlayerExtended(
-    name: pick('name').asStringOrThrow(),
-    title: pick('title').asStringOrNull(),
-    rating: pick('rating').asIntOrNull(),
-    federation: pick('fed').asStringOrNull(),
-    fideId: pick('fideId').asFideIdOrNull(),
+BroadcastPlayerWithOverallResult _makePlayerWithOverallResultFromJson(Map<String, dynamic> json) {
+  return _playerWithOverallResultFromPick(pick(json).required());
+}
+
+BroadcastPlayerWithOverallResult _playerWithOverallResultFromPick(RequiredPick pick) {
+  final player = _playerFromPick(pick);
+
+  return BroadcastPlayerWithOverallResult(
+    player: player,
     played: pick('played').asIntOrThrow(),
     score: pick('score').asDoubleOrNull(),
     ratingDiff: pick('ratingDiff').asIntOrNull(),
@@ -226,11 +232,11 @@ BroadcastPlayerExtended _playerExtendedFromPick(RequiredPick pick) {
   );
 }
 
-BroadcastPlayerResults _makePlayerResultsFromJson(Map<String, dynamic> json) {
+BroadcastPlayerWithGameResults _makePlayerWithGameResultsFromJson(Map<String, dynamic> json) {
   return (
-    player: _playerExtendedFromPick(pick(json).required()),
+    player: _playerWithOverallResultFromPick(pick(json).required()),
     fideData: _fideDataFromPick(pick(json, 'fide')),
-    games: pick(json, 'games').asListOrThrow(_makePlayerResultFromPick).toIList(),
+    games: pick(json, 'games').asListOrThrow(_playerGameResultFromPick).toIList(),
   );
 }
 
@@ -245,7 +251,7 @@ BroadcastFideData _fideDataFromPick(Pick pick) {
   );
 }
 
-BroadcastPlayerResultData _makePlayerResultFromPick(RequiredPick pick) {
+BroadcastPlayerGameResult _playerGameResultFromPick(RequiredPick pick) {
   final pointsString = pick('points').asStringOrNull();
   BroadcastPoints? points;
   if (pointsString == '1') {
@@ -256,16 +262,12 @@ BroadcastPlayerResultData _makePlayerResultFromPick(RequiredPick pick) {
     points = BroadcastPoints.zero;
   }
 
-  return BroadcastPlayerResultData(
+  return BroadcastPlayerGameResult(
     roundId: pick('round').asBroadcastRoundIdOrThrow(),
     gameId: pick('id').asBroadcastGameIdOrThrow(),
     color: pick('color').asSideOrThrow(),
     ratingDiff: pick('ratingDiff').asIntOrNull(),
     points: points,
-    opponent: _playerFromPick(
-      pick('opponent').required(),
-      isPlaying: false,
-      thinkTime: Duration.zero,
-    ),
+    opponent: _playerFromPick(pick('opponent').required()),
   );
 }

lib/src/view/broadcast/broadcast_boards_tab.dart
@@ -308,7 +308,9 @@ class _PlayerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final player = game.players[side]!;
+    final playerWithClock = game.players[side]!;
+    final player = playerWithClock.player;
+    final clock = playerWithClock.clock;
     final isClockActive = game.isOngoing && side == playingSide;
 
     return SizedBox(
@@ -321,22 +323,16 @@ class _PlayerWidget extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                child: BroadcastPlayerWidget(
-                  federation: player.federation,
-                  title: player.title,
-                  name: player.name,
-                ),
-              ),
+              Expanded(child: BroadcastPlayerWidget(player: player, showRating: false)),
               const SizedBox(width: 5),
               if (game.isOver)
                 Text(
                   game.status.resultToString(side),
                   style: const TextStyle().copyWith(fontWeight: FontWeight.bold),
                 )
-              else if (player.clock != null)
+              else if (clock != null)
                 CountdownClockBuilder(
-                  timeLeft: player.clock!,
+                  timeLeft: clock,
                   active: isClockActive,
                   builder:
                       (context, timeLeft) => Text(

lib/src/view/broadcast/broadcast_game_screen.dart
@@ -430,8 +430,9 @@ class _PlayerWidget extends ConsumerWidget {
           _PlayerWidgetPosition.top => broadcastAnalysisState.pov.opposite,
         };
 
-        final player = game.players[side]!;
-        final liveClock = isCursorOnLiveMove ? player.clock : null;
+        final playerWithClock = game.players[side]!;
+        final player = playerWithClock.player;
+        final liveClock = isCursorOnLiveMove ? playerWithClock.clock : null;
         final isClockActive = isCursorOnLiveMove && game.isOngoing && side == sideToMove;
 
         final pastClocks = broadcastAnalysisState.clocks;
@@ -441,20 +442,8 @@ class _PlayerWidget extends ConsumerWidget {
           onTap: () {
             Navigator.of(context).push(
               (tournamentId != null)
-                  ? BroadcastPlayerResultsScreen.buildRoute(
-                    context,
-                    tournamentId!,
-                    (player.fideId != null) ? player.fideId!.toString() : player.name,
-                    playerTitle: player.title,
-                    playerName: player.name,
-                  )
-                  : BroadcastPlayerResultsScreenLoading.buildRoute(
-                    context,
-                    roundId,
-                    (player.fideId != null) ? player.fideId!.toString() : player.name,
-                    playerTitle: player.title,
-                    playerName: player.name,
-                  ),
+                  ? BroadcastPlayerResultsScreen.buildRoute(context, tournamentId!, player)
+                  : BroadcastPlayerResultsScreenLoading.buildRoute(context, roundId, player),
             );
           },
           child: Container(
@@ -471,10 +460,7 @@ class _PlayerWidget extends ConsumerWidget {
                 ],
                 Expanded(
                   child: BroadcastPlayerWidget(
-                    federation: player.federation,
-                    title: player.title,
-                    name: player.name,
-                    rating: player.rating,
+                    player: player,
                     textStyle: const TextStyle().copyWith(fontWeight: FontWeight.bold),
                   ),
                 ),

lib/src/view/broadcast/broadcast_player_widget.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lichess_mobile/src/model/broadcast/broadcast.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
 
 class BroadcastPlayerWidget extends ConsumerWidget {
   const BroadcastPlayerWidget({
-    this.federation,
-    required this.title,
-    required this.name,
-    this.rating,
+    required this.player,
+    this.showFederation = true,
+    this.showRating = true,
     this.textStyle,
   });
 
-  final String? federation;
-  final String? title;
-  final int? rating;
-  final String name;
+  final BroadcastPlayer player;
+  final bool showFederation;
+  final bool showRating;
   final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final federation = player.federation;
+    final title = player.title;
+    final name = player.name;
+    final rating = player.rating;
+
     return Row(
       children: [
         if (federation != null) ...[
@@ -27,7 +31,7 @@ class BroadcastPlayerWidget extends ConsumerWidget {
         ],
         if (title != null) ...[
           Text(
-            title!,
+            title,
             style: TextStyle(
               color: (title == 'BOT') ? context.lichessColors.fancy : context.lichessColors.brag,
               fontWeight: FontWeight.bold,

commit 293e2393c35c16dff45cb20854bb08b636bf82e3

Fix broadcast player results screen showing the player instead of the opponent in the list of game results.
Bug introduced in commit b50bcae5dd70c3dd732398b8b3a6589f26a3decb.

lib/src/view/broadcast/broadcast_player_results_screen.dart
@@ -97,7 +97,6 @@ class _Body extends ConsumerWidget {
     switch (playerWithGameResults) {
       case AsyncData(value: final playerWithGameResults):
         final games = playerWithGameResults.games;
-        final player = playerWithGameResults.playerWithOverallResult.player;
 
         final showRatingDiff = games.any((result) => result.ratingDiff != null);
         final indexWidth = max(8.0 + games.length.toString().length * 10.0, 28.0);
@@ -113,7 +112,6 @@ class _Body extends ConsumerWidget {
 
             return _GameResultRow(
               playerGameResult: playerGameResult,
-              player: player,
               tournamentId: tournamentId,
               index: index,
               indexWidth: indexWidth,
@@ -251,7 +249,6 @@ class _OverallStatPlayer extends StatelessWidget {
 class _GameResultRow extends StatelessWidget {
   const _GameResultRow({
     required this.playerGameResult,
-    required this.player,
     required this.tournamentId,
     required this.index,
     required this.indexWidth,
@@ -259,7 +256,6 @@ class _GameResultRow extends StatelessWidget {
   });
 
   final BroadcastPlayerGameResult playerGameResult;
-  final BroadcastPlayer player;
   final BroadcastTournamentId tournamentId;
   final int index;
   final double indexWidth;
@@ -297,7 +293,7 @@ class _GameResultRow extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(flex: 5, child: BroadcastPlayerWidget(player: player, showRating: false)),
+              Expanded(flex: 5, child: BroadcastPlayerWidget(player: opponent, showRating: false)),
               Expanded(
                 flex: 3,
                 child: