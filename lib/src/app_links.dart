455e249e5ef019ee4d97cff85917358bcf03eda5
Vincent Velociter <vincent.velociter@gmail.com>
2025-05-30 18:29:17 +0200

    Remove archived game screen

--- lib/src/app_links.dart
+++ lib/src/app_links.dart
@@ -1,11 +1,12 @@
 import 'package:dartchess/dartchess.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lichess_mobile/src/model/analysis/analysis_controller.dart';
 import 'package:lichess_mobile/src/model/common/id.dart';
 import 'package:lichess_mobile/src/model/puzzle/puzzle_angle.dart';
+import 'package:lichess_mobile/src/view/analysis/analysis_screen.dart';
 import 'package:lichess_mobile/src/view/broadcast/broadcast_game_screen.dart';
 import 'package:lichess_mobile/src/view/broadcast/broadcast_round_screen.dart';
-import 'package:lichess_mobile/src/view/game/archived_game_screen.dart';
 import 'package:lichess_mobile/src/view/puzzle/puzzle_screen.dart';
 import 'package:lichess_mobile/src/view/study/study_screen.dart';
 import 'package:lichess_mobile/src/view/tournament/tournament_screen.dart';
@@ -42,10 +43,13 @@ Route<dynamic>? resolveAppLinkUri(BuildContext context, Uri appLinkUri) {
       final orientation = appLinkUri.pathSegments.getOrNull(2);
       // The game id can also be a challenge. Challenge by link is not supported yet so let's ignore it.
       if (gameId.isValid) {
-        return ArchivedGameScreen.buildRoute(
+        return AnalysisScreen.buildRoute(
           context,
-          gameId: gameId,
-          orientation: orientation == 'black' ? Side.black : Side.white,
+          AnalysisOptions(
+            orientation: orientation == 'black' ? Side.black : Side.white,
+            gameId: gameId,
+            initialMoveCursor: 0,
+          ),
         );
       }
   }
