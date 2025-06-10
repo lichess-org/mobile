95b49994b548f45e533f14f82ac7ff0ff9334c66
Munibullah Shah <49975985+MunibullahShah@users.noreply.github.com>
2025-06-10 16:40:19 +0500

    Removed keys in game_screen.dart

--- lib/src/view/game/game_screen.dart
+++ lib/src/view/game/game_screen.dart
@@ -106,8 +106,6 @@ enum _GameSource { lobby, challenge, game }
 
 class _GameScreenState extends ConsumerState<GameScreen> {
   final _whiteClockKey = GlobalKey(debugLabel: 'whiteClockOnGameScreen');
-  final _blackClockKey = GlobalKey(debugLabel: 'blackClockOnGameScreen');
-  final _boardKey = GlobalKey(debugLabel: 'boardOnGameScreen');
 
   @override
   void initState() {
