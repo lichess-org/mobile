8d77397e9c9fbf97b6507bc201d108ac6b3897ff
Vincent Velociter <423393+veloce@users.noreply.github.com>
2025-07-28 16:47:47 +0200

    Revert "Upload libapp.so debug symbols to Play Console (#1983)" (#2004)
    This reverts commit 915e41c7cd650fc8007ed5c59ad4481a1e0ed584.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,6 +64,9 @@ android {
                 "proguard-rules.pro"
             )
             signingConfig = signingConfigs.getByName("release")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
         debug {
             applicationIdSuffix = ".debug"
