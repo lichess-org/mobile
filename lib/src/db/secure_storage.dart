aea254aef1437c6f17bf94f32a523d39b6633f59
Vincent Velociter <vincent.velociter@gmail.com>
2025-08-08 16:58:29 +0200

    Remove deprecation warning

--- lib/src/db/secure_storage.dart
+++ lib/src/db/secure_storage.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-AndroidOptions _getAndroidOptions() => const AndroidOptions(
-  encryptedSharedPreferences: true,
-  sharedPreferencesName: 'org.lichess.mobile.secure',
-);
+AndroidOptions _getAndroidOptions() =>
+    const AndroidOptions(sharedPreferencesName: 'org.lichess.mobile.secure');
 
 class SecureStorage extends FlutterSecureStorage {
   const SecureStorage._({super.aOptions});
