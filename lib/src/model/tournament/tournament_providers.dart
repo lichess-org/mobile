74dd1809d9e7e7667f8585c5ec9891f2f7daa04c
Noah <78898963+HaonRekcef@users.noreply.github.com>
2025-08-13 09:48:48 +0200

    address code review

--- lib/src/model/tournament/tournament_providers.dart
+++ lib/src/model/tournament/tournament_providers.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lichess_mobile/src/model/common/id.dart';
 import 'package:lichess_mobile/src/model/tournament/tournament.dart';
 import 'package:lichess_mobile/src/model/tournament/tournament_repository.dart';
+import 'package:lichess_mobile/src/network/http.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'tournament_providers.g.dart';
@@ -16,4 +18,12 @@ Future<IList<LightTournament>> featuredTournaments(Ref ref) {
 @riverpod
 Future<TournamentLists> tournaments(Ref ref) {
   return ref.read(tournamentRepositoryProvider).getTournaments();
+}
+
+@riverpod
+Future<TournamentPlayer> tournamentPlayer(Ref ref, TournamentId tournamentId, UserId userId) {
+  return ref.withClientCacheFor(
+    (client) => ref.read(tournamentRepositoryProvider).getTournamentPlayer(tournamentId, userId),
+    const Duration(seconds: 10),
+  );
 }
