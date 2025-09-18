0804bbb3d0204862dad136f2ae112762f7918348
Noah <78898963+HaonRekcef@users.noreply.github.com>
2025-09-18 19:29:21 +0200

    handle crazyhouse deeplinks (#2202)

--- test/view/analysis/analysis_screen_test.dart
+++ test/view/analysis/analysis_screen_test.dart
@@ -961,6 +961,7 @@ void main() {
       );
 
       await tester.pumpWidget(app);
+      await tester.pump();
 
       await switchToPremoveTab(tester);
 
@@ -1052,6 +1053,7 @@ void main() {
       );
 
       await tester.pumpWidget(app);
+      await tester.pump();
 
       await switchToPremoveTab(tester);
 
@@ -1195,6 +1197,7 @@ void main() {
       );
 
       await tester.pumpWidget(app);
+      await tester.pump();
 
       await switchToPremoveTab(tester);
 
@@ -1263,6 +1266,7 @@ void main() {
       );
 
       await tester.pumpWidget(app);
+      await tester.pump();
 
       await switchToPremoveTab(tester);
 
