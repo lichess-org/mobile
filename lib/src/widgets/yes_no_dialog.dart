ef8c03fbf2978e23179c5e414d4147a15e082df4
erik-mor <14erto14@gmail.com>
2025-09-27 21:43:36 +0200

    fix formatting

--- lib/src/widgets/yes_no_dialog.dart
+++ lib/src/widgets/yes_no_dialog.dart
@@ -3,13 +3,7 @@ import 'package:lichess_mobile/src/utils/l10n_context.dart';
 import 'package:lichess_mobile/src/widgets/platform_alert_dialog.dart';
 
 class YesNoDialog extends StatelessWidget {
-  const YesNoDialog({
-    super.key,
-    this.title,
-    this.content,
-    required this.onYes,
-    required this.onNo,
-  });
+  const YesNoDialog({super.key, this.title, this.content, required this.onYes, required this.onNo});
 
   final Widget? title;
   final Widget? content;
