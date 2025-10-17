lichess-org/mobile

commit c65101ea6cd9342a4c30557ca34e228a22c0a4ec

fix: don't try to account for alternative castling notation in annotations (#2255)
* fix: don't try to account for alternative castling notation in annotations

This leads annotations being displayed on the wrong square e.g. when
moving the rook from e1 to h1 (as its incorecctly interpreted as a
castling move).

Luckily, we already convert alternative castling notation when constructing the
node tree, so instead of extending the logic in the AnalysisBoard, we
can just get rid of this logic entirely.

Fixes #2231

* add regression test

lib/src/view/analysis/analysis_board.dart
@@ -6,7 +6,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/constants.dart';
 import 'package:lichess_mobile/src/model/analysis/common_analysis_prefs.dart';
 import 'package:lichess_mobile/src/model/analysis/common_analysis_state.dart';
-import 'package:lichess_mobile/src/model/common/chess.dart';
 import 'package:lichess_mobile/src/model/common/eval.dart';
 import 'package:lichess_mobile/src/model/engine/evaluation_preferences.dart';
 import 'package:lichess_mobile/src/model/engine/evaluation_service.dart';
@@ -124,9 +123,7 @@ abstract class AnalysisBoardState<
           : null,
       shapes: userShapes.union(_bestMoveShapes(boardPrefs.pieceSet.assets)).union(extraShapes),
       annotations: sanMove != null && annotation != null
-          ? altCastles.containsKey(sanMove.move.uci)
-                ? IMap({Move.parse(altCastles[sanMove.move.uci]!)!.to: annotation})
-                : IMap({sanMove.move.to: annotation})
+          ? IMap({sanMove.move.to: annotation})
           : null,
       settings: boardPrefs.toBoardSettings().copyWith(
         borderRadius: widget.boardRadius,

test/view/study/study_screen_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:chessground/chessground.dart';
 import 'package:dartchess/dartchess.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/widgets.dart';
@@ -493,5 +494,95 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.text('2. Illegal Chapter'), findsOneWidget);
     });
+  });
+
+  testWidgets('Displays annotations on the correct square', (WidgetTester tester) async {
+    final mockRepository = MockStudyRepository();
+    when(() => mockRepository.getStudy(id: testId)).thenAnswer(
+      (_) async => (
+        makeStudy(
+          chapter: makeChapter(
+            id: const StudyChapterId('1'),
+            orientation: Side.white,
+            gamebook: false,
+          ),
+        ),
+        '''
+[Event "annotation bug test: Chapter 2"]
+[Date "2025.09.28"]
+[Result "*"]
+[Variant "Standard"]
+[ECO "?"]
+[Opening "?"]
+[StudyName "annotation bug test"]
+[ChapterName "Chapter 2"]
+[ChapterURL "https://lichess.org/study/EQWia70B/M289O12T"]
+[Annotator "https://lichess.org/@/yassine00"]
+[FEN "8/8/2k5/8/8/8/8/1K3R2 b - - 5 3"]
+[SetUp "1"]
+[UTCDate "2025.09.28"]
+[UTCTime "14:16:01"]
+
+3... Kd5 4. Rg1!! Kd4 5. Re1 Kc4 6. Rh1?? *
+''',
+      ),
+    );
+
+    final app = await makeTestProviderScopeApp(
+      tester,
+      home: const StudyScreen(id: testId),
+      overrides: [studyRepositoryProvider.overrideWith((ref) => mockRepository)],
+    );
+    await tester.pumpWidget(app);
+    // Wait for study to load
+    await tester.pumpAndSettle();
+
+    void expectAnnotations(Iterable<Matcher> annotations) {
+      final board = tester.widget<Chessboard>(find.byType(Chessboard));
+
+      if (annotations.isEmpty) {
+        expect(board.annotations, isNull);
+      } else {
+        expect(board.annotations!.length, annotations.length);
+        expect(board.annotations, allOf(annotations));
+      }
+    }
+
+    expectAnnotations([]);
+
+    // each pumpAndSettle() call waits for move to be played and potential annotation to appear
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle();
+    expectAnnotations([]);
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle();
+
+    // 4. Rg1!!
+    expectAnnotations([
+      containsPair(Square.g1, predicate<Annotation>((annotation) => annotation.symbol == '!!')),
+    ]);
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle(); // Wait for move to be played
+    expectAnnotations([]);
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle(); // Wait for move to be played
+    expectAnnotations([]);
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle(); // Wait for move to be played
+    expectAnnotations([]);
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle(); // Wait for move to be played
+
+    // Regression test for https://github.com/lichess-org/mobile/issues/2231
+    // 6. Rh1??
+    expectAnnotations([
+      containsPair(Square.h1, predicate<Annotation>((annotation) => annotation.symbol == '??')),
+    ]);
   });
 }

commit 874eb0e07f205a5d613edf3677d2eea863983cea

fix regression caused by #2255
I wrongly assumed that addMoveAt() already converts castling notation
for us, but that function is only called when new nodes are added to the
move tree in an active game, but not when parsing a study chapter.

This is why #2255 fixed the bug of a mve being incorrectly interpreted
as alt-castling, but it now broke the annotations if the move actually
WAS a castling move.

Luckily, while in addMoveAt() there are some heuristics because we only
have the UCI move, not the SAN move, in the AnalysisBoard we *do* have
the SAN move, so we can just check for O-O/O-O-O to avoid false
positives.

Resolves #2231

lib/src/view/analysis/analysis_board.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/constants.dart';
 import 'package:lichess_mobile/src/model/analysis/common_analysis_prefs.dart';
 import 'package:lichess_mobile/src/model/analysis/common_analysis_state.dart';
+import 'package:lichess_mobile/src/model/common/chess.dart';
 import 'package:lichess_mobile/src/model/common/eval.dart';
 import 'package:lichess_mobile/src/model/engine/evaluation_preferences.dart';
 import 'package:lichess_mobile/src/model/engine/evaluation_service.dart';
@@ -123,7 +124,10 @@ abstract class AnalysisBoardState<
           : null,
       shapes: userShapes.union(_bestMoveShapes(boardPrefs.pieceSet.assets)).union(extraShapes),
       annotations: sanMove != null && annotation != null
-          ? IMap({sanMove.move.to: annotation})
+          ? (sanMove.san == 'O-O' || sanMove.san == 'O-O-O') &&
+                    altCastles.containsKey(sanMove.move.uci)
+                ? IMap({Move.parse(altCastles[sanMove.move.uci]!)!.to: annotation})
+                : IMap({sanMove.move.to: annotation})
           : null,
       settings: boardPrefs.toBoardSettings().copyWith(
         borderRadius: widget.boardRadius,

test/view/study/study_screen_test.dart
@@ -585,4 +585,52 @@ void main() {
       containsPair(Square.h1, predicate<Annotation>((annotation) => annotation.symbol == '??')),
     ]);
   });
+
+  testWidgets('Displays correct annotation for castling on correct square', (
+    WidgetTester tester,
+  ) async {
+    final mockRepository = MockStudyRepository();
+    when(() => mockRepository.getStudy(id: testId)).thenAnswer(
+      (_) async => (
+        makeStudy(
+          chapter: makeChapter(
+            id: const StudyChapterId('1'),
+            orientation: Side.white,
+            gamebook: false,
+          ),
+        ),
+        '''
+[Event "Test castling annotations"]
+[Date "2025.09.28"]
+[Result "*"]
+[Variant "Standard"]
+[ECO "?"]
+[Opening "?"]
+[StudyName "Test castling annotations"]
+[FEN "r3kb1r/p2nqppp/5n2/1B2p1B1/4P3/1Q6/PPP2PPP/R3K2R w KQkq - 1 12"]
+
+12. O-O-O!!
+''',
+      ),
+    );
+
+    final app = await makeTestProviderScopeApp(
+      tester,
+      home: const StudyScreen(id: testId),
+      overrides: [studyRepositoryProvider.overrideWith((ref) => mockRepository)],
+    );
+    await tester.pumpWidget(app);
+    // Wait for study to load
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byTooltip('Next'));
+    await tester.pumpAndSettle(); // Wait for O-O-O move to be played
+
+    final board = tester.widget<Chessboard>(find.byType(Chessboard));
+    expect(board.annotations!.length, 1);
+    expect(
+      board.annotations,
+      containsPair(Square.c1, predicate<Annotation>((annotation) => annotation.symbol == '!!')),
+    );
+  });
 }