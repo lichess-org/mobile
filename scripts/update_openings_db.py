b67c4cc22a3dcc1d984a44da92c38e0ad4c26fa3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-10-30 20:23:10 -0400

    modify the db only if all.tsv is ok

--- scripts/update_openings_db.py
+++ scripts/update_openings_db.py
@@ -17,16 +17,19 @@ if not os.path.isdir(path_to_chess_openings):
     sys.exit(1)
 
 subprocess.run(["make", "all"], cwd=path_to_chess_openings, check=True)
+
 db_path = os.path.join(os.path.dirname(__file__), '../assets/chess_openings.db')
 conn = sqlite3.connect(db_path)
-cur = conn.cursor()
-cur.execute('DELETE FROM openings;')
-conn.commit()
 
 with open(os.path.join(path_to_chess_openings, 'dist/all.tsv'), 'r') as f:
     dr = csv.DictReader(f, delimiter='\t')
     to_db = [(i['eco'], i['name'], i['pgn'], i['uci'], i['epd']) for i in dr]
 
-cur.executemany('INSERT INTO openings (eco, name, pgn, uci, epd) VALUES (?, ?, ?, ?, ?);', to_db)
-conn.commit()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM openings;')
+    conn.commit()
+
+    cur.executemany('INSERT INTO openings (eco, name, pgn, uci, epd) VALUES (?, ?, ?, ?, ?);', to_db)
+    conn.commit()
+
 conn.close()
