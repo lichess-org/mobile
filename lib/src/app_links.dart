da94aa6ad976b6a4bf82da7b686c87339a5f45a3
Noah <78898963+HaonRekcef@users.noreply.github.com>
2026-01-31 21:47:10 +0100

    add app_links

--- lib/src/app_links.dart
+++ lib/src/app_links.dart
@@ -14,50 +14,59 @@ import 'package:lichess_mobile/src/view/study/study_screen.dart';
 import 'package:lichess_mobile/src/view/tournament/tournament_screen.dart';
 import 'package:lichess_mobile/src/view/user/user_or_profile_screen.dart';
 import 'package:linkify/linkify.dart';
+import 'package:logging/logging.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-/// Resolves an app link [Uri] to a corresponding [Route].
-Route<dynamic>? resolveAppLinkUri(BuildContext context, Uri appLinkUri) {
-  if (appLinkUri.pathSegments.isEmpty) return null;
+final _logger = Logger('AppLinks');
 
+/// Resolves an app link [Uri] to a corresponding [Route].
+List<Route<dynamic>>? resolveAppLinkUri(BuildContext context, Uri appLinkUri) {
+  if (appLinkUri.pathSegments.isEmpty) return null;
+  _logger.info('Resolving app link: $appLinkUri');
   switch (appLinkUri.pathSegments[0]) {
     case 'study':
       final id = appLinkUri.pathSegments[1];
-      return StudyScreen.buildRoute(context, StudyId(id));
+      return [StudyScreen.buildRoute(context, StudyId(id))];
     case 'broadcast':
       final roundId = BroadcastRoundId(appLinkUri.pathSegments[3]);
       if (appLinkUri.pathSegments.length > 4) {
         final gameId = BroadcastGameId(appLinkUri.pathSegments[4]);
-        return BroadcastGameScreen.buildRoute(context, roundId: roundId, gameId: gameId);
+        return [
+          BroadcastRoundScreenLoading.buildRoute(
+            context,
+            roundId,
+            initialTab: BroadcastRoundTab.boards,
+          ),
+          BroadcastGameScreen.buildRoute(context, roundId: roundId, gameId: gameId),
+        ];
       } else {
         final tab = BroadcastRoundTab.tabOrNullFromString(appLinkUri.fragment);
-        return BroadcastRoundScreenLoading.buildRoute(context, roundId, initialTab: tab);
+        return [BroadcastRoundScreenLoading.buildRoute(context, roundId, initialTab: tab)];
       }
     case 'tournament':
       final tournamentId = TournamentId(appLinkUri.pathSegments[1]);
-      return TournamentScreen.buildRoute(context, tournamentId);
-
+      return [TournamentScreen.buildRoute(context, tournamentId)];
     case 'training':
       final id = appLinkUri.pathSegments[1];
-      return PuzzleScreen.buildRoute(
-        context,
-        angle: PuzzleAngle.fromKey('mix'),
-        puzzleId: PuzzleId(id),
-      );
+      return [
+        PuzzleScreen.buildRoute(context, angle: PuzzleAngle.fromKey('mix'), puzzleId: PuzzleId(id)),
+      ];
     case _:
       final gameId = GameId(appLinkUri.pathSegments[0]);
       final orientation = appLinkUri.pathSegments.getOrNull(1);
       final int ply = int.tryParse(appLinkUri.fragment) ?? 0;
       // The game id can also be a challenge. Challenge by link is not supported yet so let's ignore it.
       if (gameId.isValid) {
-        return AnalysisScreen.buildRoute(
-          context,
-          AnalysisOptions.archivedGame(
-            orientation: orientation == 'black' ? Side.black : Side.white,
-            gameId: gameId,
-            initialMoveCursor: ply,
+        return [
+          AnalysisScreen.buildRoute(
+            context,
+            AnalysisOptions.archivedGame(
+              orientation: orientation == 'black' ? Side.black : Side.white,
+              gameId: gameId,
+              initialMoveCursor: ply,
+            ),
           ),
-        );
+        ];
       }
   }
 
@@ -71,9 +80,11 @@ void onLinkifyOpen(BuildContext context, LinkableElement link) {
   if (link is UrlElement && link.url.startsWith(RegExp('https?:\\/\\/$kLichessHost'))) {
     // Handle Lichess links specifically
     final appLinkUri = Uri.parse(link.url);
-    final route = resolveAppLinkUri(context, appLinkUri);
-    if (route != null) {
-      Navigator.of(context).push(route);
+    final routes = resolveAppLinkUri(context, appLinkUri);
+    if (routes != null) {
+      for (final route in routes) {
+        Navigator.of(context).push(route);
+      }
     } else {
       // If the link is not recognized, open it in a browser
       launchUrl(appLinkUri);
