5dc1d7602174a5bacc1accd5c25b1e011ea0d2f8
Rafal Sroka <rafal.sroka.it@gmail.com>
2026-03-22 22:34:18 +0100

    Remove news feed

--- ios/LichessWidgets/Blog Feed Widget/Model/BlogFeedChoice.swift
+++ ios/LichessWidgets/Blog Feed Widget/Model/BlogFeedChoice.swift
@@ -3,7 +3,6 @@ import AppIntents
 
 enum BlogFeedChoice: String, AppEnum {
     case officialBlog
-    case newsFeed
     case communityBlog
     case userBlog
 
@@ -11,7 +10,6 @@ enum BlogFeedChoice: String, AppEnum {
 
     static var caseDisplayRepresentations: [BlogFeedChoice: DisplayRepresentation] = [
         .officialBlog: "Official Blog",
-        .newsFeed: "News Feed",
         .communityBlog: "Community Blog",
         .userBlog: "User Blog",
     ]
@@ -19,7 +17,6 @@ enum BlogFeedChoice: String, AppEnum {
     func feedURL(username: String?) -> String? {
         switch self {
         case .officialBlog: return "https://lichess.org/@/Lichess/blog.atom"
-        case .newsFeed: return "https://lichess.org/feed.atom"
         case .communityBlog: return "https://lichess.org/blog/community.atom"
         case .userBlog:
             guard let username, !username.isEmpty else { return nil }
