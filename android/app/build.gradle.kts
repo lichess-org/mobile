c8624aac26950b03646c3880451324aa4c823a20
Julien <120588494+julien4215@users.noreply.github.com>
2026-03-23 16:59:09 +0100

    Split APK per ABI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -100,3 +100,14 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("androidx.core:core-splashscreen:1.0.1")
 }
+
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
+android.applicationVariants.configureEach {
+    val variant = this
+    variant.outputs.forEach { output ->
+        val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+        if (abiVersionCode != null) {
+            (output as ApkVariantOutputImpl).versionCodeOverride = variant.versionCode * 100 + abiVersionCode
+        }
+    }
+}
