1b5f834b138647b6c3cbc25d74de55e4a29df4d4
Rafal Sroka <rafal.sroka.it@gmail.com>
2026-04-09 20:45:43 +0200

    Document kid mode key

--- ios/LichessWidgets/LichessAppGroup.swift
+++ ios/LichessWidgets/LichessAppGroup.swift
@@ -3,7 +3,10 @@ import Foundation
 enum LichessAppGroup {
     static let id = "group.org.lichess.mobileV2"
 
+    // Key must match the string passed to HomeWidget.saveWidgetData in lib/src/app.dart.
+    static let kidModeKey = "isKidMode"
+
     static var isKidModeActive: Bool {
-        UserDefaults(suiteName: id)?.bool(forKey: "isKidMode") ?? false
+        UserDefaults(suiteName: id)?.bool(forKey: kidModeKey) ?? false
     }
 }
