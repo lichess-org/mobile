b7d6cf171e07005f7fe68d0d1ae63a4674d92ba2
Vincent Velociter <vincent.velociter@gmail.com>
2026-04-14 12:18:42 +0200

    Set the blur flag if app was in background since prev move

--- lib/src/model/game/game_controller.dart
+++ lib/src/model/game/game_controller.dart
@@ -66,6 +66,9 @@ class GameController extends AsyncNotifier<GameState> {
 
   final _onFlagThrottler = Throttler(const Duration(milliseconds: 500));
 
+  /// Whether the app was put in background since the previous move was sent.
+  bool _wasInBackground = false;
+
   static Uri socketUri(GameFullId gameFullId) => Uri(path: '/play/$gameFullId/v6');
 
   SocketPool get _socketPool => ref.read(socketPoolProvider);
@@ -148,6 +151,7 @@ class GameController extends AsyncNotifier<GameState> {
     }
     // real time games need the socket to stay connected otherwise lichess will think the player leaved
     // correspondence games can and should close the socket when the app is in background (because lichess won't send the push notification update when the player is still connected to the socket)
+    _wasInBackground = true;
     if (state.requireValue.game.meta.speed == Speed.correspondence) {
       _socketClient.close();
     }
@@ -522,11 +526,14 @@ class GameController extends AsyncNotifier<GameState> {
       DropMove(:final role, :final to) => ('drop', {'role': role.name, 'pos': to.name}),
     };
 
+    final inBackground = _wasInBackground;
+    _wasInBackground = false;
     _socketClient.send(
       topic,
       {
         ...data,
         if (moveTime != null) 's': (moveTime.inMilliseconds * 0.1).round().toRadixString(36),
+        if (inBackground) 'b': 1,
       },
       ackable: true,
       withLag: _clock != null && (moveTime == null || withLag),
