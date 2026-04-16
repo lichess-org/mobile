3a8904a8dd463babaea4539e31e11038af3a88ff
Noah <noah.fecker@gmail.com>
2026-04-16 19:57:26 +0200

    fix more issues when changing groups

--- lib/src/view/broadcast/broadcast_round_screen.dart
+++ lib/src/view/broadcast/broadcast_round_screen.dart
@@ -192,7 +192,10 @@ class _BroadcastRoundScreenState extends ConsumerState<BroadcastRoundScreen>
       AsyncData(value: final roundState) => PlatformScaffold(
         extendBody: Theme.of(context).platform == TargetPlatform.iOS,
         appBar: PlatformAppBar(
-          title: AppBarTitleText(widget.broadcast.title, maxLines: 2),
+          title: AppBarTitleText(
+            asyncTournament.value?.data.name ?? widget.broadcast.title,
+            maxLines: 2,
+          ),
           bottom: TabBar(
             controller: _tabController,
             tabs: <Widget>[
@@ -234,7 +237,7 @@ class _BroadcastRoundScreenState extends ConsumerState<BroadcastRoundScreen>
               AsyncData(:final value) => BroadcastBoardsTab(
                 tournamentId: _selectedTournamentId,
                 roundId: _selectedRoundId ?? value.defaultRoundId,
-                tournamentSlug: widget.broadcast.tour.slug,
+                tournamentSlug: value.data.slug,
                 showOnlyOngoingGames: filter == _BroadcastGameFilter.ongoing,
               ),
               _ => const SizedBox.shrink(),
@@ -245,7 +248,7 @@ class _BroadcastRoundScreenState extends ConsumerState<BroadcastRoundScreen>
                 AsyncData(:final value) => BroadcastTeamsTab(
                   roundId: _selectedRoundId ?? value.defaultRoundId,
                   tournamentId: _selectedTournamentId,
-                  tournamentSlug: widget.broadcast.tour.slug,
+                  tournamentSlug: value.data.slug,
                 ),
                 _ => const SizedBox.shrink(),
               },
