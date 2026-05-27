f6d37fe48e46762f750a198db98fee8e0610a634
Vincent Velociter <vincent.velociter@gmail.com>
2026-05-27 11:21:37 +0200

    Replace deprecated package with its continuation

--- lib/src/view/broadcast/broadcast_overview_tab.dart
+++ lib/src/view/broadcast/broadcast_overview_tab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:lichess_mobile/src/model/broadcast/broadcast.dart';
