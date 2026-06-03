6a85daed0bb30eac21a895292a15752113dce2ed
Vincent Velociter <vincent.velociter@gmail.com>
2026-06-03 16:07:43 +0200

    Remove useless widget

--- lib/src/view/engine/engine_gauge.dart
+++ lib/src/view/engine/engine_gauge.dart
@@ -167,14 +167,9 @@ class _EvalGaugeState extends State<_EvalGauge> {
                             : Alignment.bottomCenter,
                         child: Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 1.0, vertical: 3.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              FittedBox(
-                                fit: BoxFit.scaleDown,
-                                child: Text(evalDisplay, style: evalStyle),
-                              ),
-                            ],
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(evalDisplay, style: evalStyle),
                           ),
                         ),
                       )
