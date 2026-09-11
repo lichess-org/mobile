c5c7f2c83f2450dd525fe01e0375e86c3c61d480
Vincent Velociter <vincent.velociter@gmail.com>
2026-09-11 12:32:04 -0500

    Explicitely falls back to bundled maia when weight was deleted
    Closes #3660

--- test/view/offline_computer/offline_computer_game_screen_test.dart
+++ test/view/offline_computer/offline_computer_game_screen_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:chessground/chessground.dart';
 import 'package:cupertino_ui/cupertino_ui.dart';
 import 'package:dartchess/dartchess.dart';
@@ -10,6 +12,7 @@ import 'package:lichess_mobile/src/model/common/id.dart';
 import 'package:lichess_mobile/src/model/common/perf.dart';
 import 'package:lichess_mobile/src/model/common/speed.dart';
 import 'package:lichess_mobile/src/model/engine/position_evaluator.dart';
+import 'package:lichess_mobile/src/model/engine/weights_service.dart';
 import 'package:lichess_mobile/src/model/game/game.dart';
 import 'package:lichess_mobile/src/model/game/game_status.dart';
 import 'package:lichess_mobile/src/model/game/offline_computer_game.dart';
@@ -20,6 +23,7 @@ import 'package:lichess_mobile/src/model/offline_computer/offline_computer_game_
 import 'package:lichess_mobile/src/model/offline_computer/offline_computer_game_storage.dart';
 import 'package:lichess_mobile/src/model/offline_computer/practice_analyser.dart';
 import 'package:lichess_mobile/src/model/offline_computer/practice_comment.dart';
+import 'package:lichess_mobile/src/model/settings/preferences_storage.dart';
 import 'package:lichess_mobile/src/styles/lichess_colors.dart';
 import 'package:lichess_mobile/src/utils/navigation.dart';
 import 'package:lichess_mobile/src/view/offline_computer/offline_computer_game_screen.dart';
@@ -32,6 +36,7 @@ import 'package:mocktail/mocktail.dart';
 
 import '../../binding.dart';
 import '../../model/engine/fake_engine.dart';
+import '../../model/engine/fake_weights_service.dart';
 import '../../test_helpers.dart';
 import '../../test_provider_scope.dart';
 
@@ -590,6 +595,46 @@ void main() {
 
       // Verify game started
       expect(find.byType(Chessboard), findsOneWidget);
+    });
+
+    testWidgets('A preferred Maia whose network was deleted falls back to the bundled one', (
+      tester,
+    ) async {
+      final gameStorage = MockOfflineComputerGameStorage();
+      when(() => gameStorage.fetchGame()).thenAnswer((_) async => null);
+      final weights = FakeMaiaWeightsService();
+
+      final app = await makeTestProviderScopeApp(
+        tester,
+        home: const OfflineComputerGameScreen(),
+        defaultPreferences: {
+          PrefCategory.offlineComputerGame.storageKey: jsonEncode(
+            OfflineComputerGamePrefs.defaults
+                .copyWith(opponentSpec: const MaiaOpponentSpec(MaiaRating.maia2200))
+                .toJson(),
+          ),
+        },
+        overrides: {
+          offlineComputerGameStorageProvider: offlineComputerGameStorageProvider.overrideWith(
+            (_) => gameStorage,
+          ),
+          maiaWeightsServiceProvider: maiaWeightsServiceProvider.overrideWithValue(weights),
+        },
+      );
+      await tester.pumpWidget(app);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Maia 2200'), findsNothing);
+      expect(find.text('Maia ${MaiaRating.defaultRating.rating}'), findsWidgets);
+
+      final container = ProviderScope.containerOf(
+        tester.element(find.byType(OfflineComputerGameScreen)),
+      );
+      expect(
+        container.read(offlineComputerGamePreferencesProvider).opponentSpec,
+        const MaiaOpponentSpec(MaiaRating.defaultRating),
+      );
+      expect(weights.downloads, isEmpty);
     });
 
     testWidgets('Casual switch is shown in new game dialog', (tester) async {
