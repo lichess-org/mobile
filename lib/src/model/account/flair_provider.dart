68daa9149c932398c85456c11b028f19dd2a9df5
Slick Daddy <129640104+slick-daddy@users.noreply.github.com>
2026-09-25 19:54:24 +0300

    Move the emoji picker data model out of the widget layer

--- lib/src/model/account/flair_provider.dart
+++ lib/src/model/account/flair_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lichess_mobile/src/model/emoji_picker_models.dart';
 import 'package:lichess_mobile/src/network/http.dart';
 import 'package:lichess_mobile/src/utils/lichess_assets.dart';
-import 'package:lichess_mobile/src/widgets/emoji_picker/emoji_picker_models.dart';
 
 /// A provider that fetches the list of available flairs.
 final flairListProvider = FutureProvider<EmojiData>((Ref ref) async {
--- lib/src/widgets/emoji_picker/emoji_picker.dart
+++ lib/src/widgets/emoji_picker/emoji_picker.dart
@@ -1,8 +1,8 @@
+import 'package:lichess_mobile/src/model/emoji_picker_models.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
 import 'package:lichess_mobile/src/utils/rate_limit.dart';
 import 'package:lichess_mobile/src/widgets/emoji_picker/emoji_item.dart';
 import 'package:lichess_mobile/src/widgets/emoji_picker/emoji_picker_configuration.dart';
-import 'package:lichess_mobile/src/widgets/emoji_picker/emoji_picker_models.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
